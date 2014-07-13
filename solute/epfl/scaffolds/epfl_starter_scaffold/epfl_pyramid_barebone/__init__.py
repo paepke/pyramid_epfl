a3959be2de74af8674a2ecf897192edc6a184435
Gunter Bach <gunter@bach.de>
2014-07-13 21:47:39 +0200

    refactor: barebone scaffold cleaned completely

--- solute/epfl/scaffolds/epfl_starter_scaffold/epfl_pyramid_barebone/__init__.py
+++ solute/epfl/scaffolds/epfl_starter_scaffold/epfl_pyramid_barebone/__init__.py
@@ -8,6 +8,7 @@ from pyramid_beaker import session_factory_from_settings
 from pyramid.authentication import AuthTktAuthenticationPolicy
 from pyramid.authorization import ACLAuthorizationPolicy
 
+from solute.epfl import epfltempdata
 
 def main(global_config, **settings):
     """ This function returns a Pyramid WSGI application.
@@ -23,13 +24,21 @@ def main(global_config, **settings):
     config.set_session_factory(session_factory)
 
     config.add_static_view(name = 'static', 
-                           path = 'epfl_pyramid_barebone:static', 
+                           path = 'epfl_pyramid_demo:static', 
                            cache_max_age = 3600)    
 
     authn_policy = AuthTktAuthenticationPolicy('seekrit', hashalg='sha512')
     authz_policy = ACLAuthorizationPolicy()
     config.set_authentication_policy(authn_policy)
     config.set_authorization_policy(authz_policy)
+
+
+    temp_data_provider = epfltempdata.LocalMemoryProvider(config)
+    config.set_tempdata_provider(temp_data_provider)
+
+    nodeglobal_data_provider = epfltempdata.LocalMemoryProvider(config)
+    config.set_nodeglobaldata_provider(nodeglobal_data_provider)
+
     
     config.scan()
     return config.make_wsgi_app()
