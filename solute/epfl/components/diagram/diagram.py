a3af7c6b777d63484ae06ac592e7462e8e48b35b
Michael Hauck <michael.hauck@avenyou.com>
2014-12-17 14:22:04 +0100

    added server-side state of diagram series visibility

--- solute/epfl/components/diagram/diagram.py
+++ solute/epfl/components/diagram/diagram.py
@@ -19,11 +19,11 @@ class Diagram(epflcomponentbase.ComponentBase):
     template_name = "diagram/diagram.html"
     asset_spec = "solute.epfl.components:diagram/static"
 
-    js_name = ["highcharts.js", "exporting.js", "export-csv-1.2.1.js"]
+    js_name = ["highcharts.js", "exporting.js", "export-csv-1.2.1.js", "diagram.js"]
 
     
     compo_config = []
-    compo_state = []
+    compo_state = [ "diagram_params"]
     
     diagram_params = {}
     
@@ -31,6 +31,19 @@ class Diagram(epflcomponentbase.ComponentBase):
         return self.diagram_params
     def set_params(self, params):
         self.diagram_params = params
+        
+    def handle_visibilityChange(self, series_visibility):
+        if not "series" in self.diagram_params:
+            return
+        for series_visibility_entry in series_visibility:
+             if "name" in series_visibility_entry:
+                 for backed_series_entry in self.diagram_params["series"]:
+                     if backed_series_entry["name"] == series_visibility_entry["name"]:
+                         if ("visible" in series_visibility_entry) and (series_visibility_entry["visible"] == False):
+                              backed_series_entry["visible"] = False
+                         elif "visible" in backed_series_entry:
+                             backed_series_entry.pop("visible")
+                    
 
     def __init__(self, label=None, value=None, callback=None, **extra_params):
         super(Diagram, self).__init__()
---
+++ solute/epfl/components/diagram/static/diagram.js
@@ -0,0 +1,36 @@
+epfl.DiagramComponent = function(cid, params) {
+    epfl.ComponentBase.call(this, cid, params);
+    var compo = this;
+    console.log(params);
+    $('#' + cid).highcharts(
+        params
+    );
+    // handle changes in series visibility
+    $('#' + cid + ' .highcharts-legend-item').click(function(event) {
+    	var my_chart = $('#' + cid).highcharts();
+    	series_visibility = [];
+        for (var i = 0; i < my_chart.series.length; i++) {
+		    series = my_chart.series[i];
+		    series_json = { "name": series.name };
+		    if (series.visible == false) {
+		    	series_json["visible"] = false;
+		    }
+		    series_visibility.push(series_json);
+		}
+		var ev = compo.make_event("visibilityChange",{"series_visibility":series_visibility});
+        epfl.send(ev); 
+    });
+    
+
+};
+epfl.DiagramComponent.inherits_from(epfl.ComponentBase);
+
+epfl.DiagramComponent.prototype.fire_event = function(event_name, params, callback_fn) {
+    if (!params) {
+        params = {}
+    };
+    var evt = this.make_event(event_name, params);
+    epfl.send(evt, callback_fn)
+};
+
+
