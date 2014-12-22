3b95746a1fcd8791c8c8abebee8d1088c96c6c6a
mast <mast@solute.de>
2014-12-22 12:05:32 +0100

    refactor flipflop component

--- solute/epfl/components/flipflop/static/flipflop.js
+++ solute/epfl/components/flipflop/static/flipflop.js
@@ -1,29 +1,16 @@
-epfl.DragableComponent = function(cid, params) {
+epfl.FlipFlopComponent = function(cid, params) {
     epfl.ComponentBase.call(this, cid, params);
     
     var widget_obj = this;
 
-    var children = $("#"+cid).children();
-
-    $("#" + cid).click(function(event){
-        var ev = widget_obj.make_event("onClickChildren",{"compo_id":event.target.id});
-        epfl.send(ev); 
-    });
 
 };
-epfl.DragableComponent.inherits_from(epfl.ComponentBase);
+epfl.FlipFlopComponent.inherits_from(epfl.ComponentBase);
 
-epfl.DragableComponent.prototype.fire_event = function(event_name, params, callback_fn) {
+epfl.FlipFlopComponent.prototype.fire_event = function(event_name, params, callback_fn) {
     if (!params) {
         params = {}
     };
     var evt = this.make_event(event_name, params);
     epfl.send(evt, callback_fn)
 };
-
-epfl.DragableComponent.prototype.remove_row = function(rowid) {
-    console.log("remove",rowid);
-    console.log(this.cid);
-    $("#" + this.cid + " #" + rowid).remove();
-};
-
