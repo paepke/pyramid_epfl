5b101ccbcf1ebe85a30dddc34b84aa1164ddfd57
mast <mast@solute.de>
2014-12-30 15:55:45 +0100

    add new value visual features to form

--- solute/epfl/components/form_components/form_macros.js
+++ solute/epfl/components/form_components/form_macros.js
@@ -1,11 +1,9 @@
-
 function get_typeahead(cid, query, process) {
     // TODO: fire event and query results from server
     return ['Amsterdam', 'Washington', 'Sydney', 'Beijing', 'Cairo'];
 }
 
 var selector = "#{{compo.cid}}";
-console.log($(selector));
 var type = $(selector).closest("div").attr('epfl-type');
 
 if (type == "defaultinput" || type == "textarea" || type == "select") {
@@ -32,22 +30,22 @@ if (type == "defaultinput" || type == "textarea" || type == "select") {
 } else if (type == "toggle") {
     $(selector).attr('checked', $("#{{compo.cid}}").val() == 'True');
     $(selector).bootstrapSwitch('state');
-        $(selector).on('switchChange.bootstrapSwitch', function(event, state) {
-            var val = $(this).closest("div").parent().hasClass("bootstrap-switch-on");
-            epfl.repeat_enqueue(epfl.make_component_event("{{ compo.cid }}", 'change', {value: val}), "{{ compo.cid }}");
-        });
+    $(selector).on('switchChange.bootstrapSwitch', function (event, state) {
+        var val = $(this).closest("div").parent().hasClass("bootstrap-switch-on");
+        epfl.repeat_enqueue(epfl.make_component_event("{{ compo.cid }}", 'change', {value: val}), "{{ compo.cid }}");
+    });
 
 } else if (type == "radiobuttongroup") {
     selector = "input[type=radio][name={{ compo.cid }}]";
     $(selector).change(function () {
-        var val = val = $(this).is(':checked');
+        var val = $(this).val();
         epfl.repeat_enqueue(epfl.make_component_event("{{ compo.cid }}", 'change', {value: val}), "{{ compo.cid }}");
     });
 
 } else if (type == "buttonsetgroup") {
     selector = "input[type=radio][name={{ compo.cid }}]";
     $(selector).change(function () {
-        var val = val = $(this).is(':checked');
+        var val = $(this).val();
         var parent = $(this).parent().parent();
         $(parent).find("label").removeClass("active");
         $(this).parent().addClass("active");
@@ -55,4 +53,3 @@ if (type == "defaultinput" || type == "textarea" || type == "select") {
     });
 
 }
-
