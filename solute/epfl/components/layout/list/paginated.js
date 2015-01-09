584e4ae3f467f308147a066a9d075a3b13372bdc
Justus Wingert <justus@abi007.info>
2015-01-10 00:50:02 +0100

    Implemented autosubmit with 500ms delay.

--- solute/epfl/components/layout/list/paginated.js
+++ solute/epfl/components/layout/list/paginated.js
@@ -13,12 +13,23 @@ epfl.paginated_list_goto = function (element, cid, row_offset, row_limit, row_da
     epfl.send(event);
 };
 
+
+var {{ compo.cid }}_timeout;
 $('#' + '{{ compo.cid }}_search').keypress(function (e) {
-    if (e.key == 'Enter') {
-        epfl.paginated_list_goto($(this),
+    var elm = this;
+    function submit() {
+        epfl.paginated_list_goto($(elm),
                                  "{{ compo.cid }}",
                                  parseInt({{ compo.row_offset }}),
                                  parseInt({{ compo.row_limit }}),
-                                 {search: $(this).val()});
+                                 {search: $(elm).val()});
     }
+
+    if (e.key == 'Enter') {
+        submit();
+    }
+    if ({{ compo.cid }}_timeout) {
+        clearTimeout({{ compo.cid }}_timeout);
+    }
+    {{ compo.cid }}_timeout = window.setTimeout(submit, 500);
 });
