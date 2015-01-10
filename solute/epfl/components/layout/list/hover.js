a6b4593a56d3311cb7451391fff04ad83a6f2e9a
Justus Wingert <justus@abi007.info>
2015-01-10 19:27:42 +0100

    Bugfix: added margin to hover image.

--- solute/epfl/components/layout/list/hover.js
+++ solute/epfl/components/layout/list/hover.js
@@ -7,7 +7,7 @@ $('[epflid={{ compo_obj.cid }}]').hover(function (e) {
         $('<div id="hover_element{{ compo_obj.cid }}">')
             .css('position', 'absolute')
             .css('top', e.pageY + 'px')
-            .css('left', e.pageX + 'px')
+            .css('left', (e.pageX + 10) + 'px')
             .css('z-index', '100')
             .append('<img src="{{ compo_obj.src }}" class="img-thumbnail" />')
             .appendTo(document.body);
