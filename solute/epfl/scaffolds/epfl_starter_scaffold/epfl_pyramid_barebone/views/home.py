004a0a05cc09237b360f18507a60ddfce2b2ae1f
Michael Hauck <michael.hauck@avenyou.com>
2015-01-14 18:16:11 +0100

    scaffold cleanup

--- solute/epfl/scaffolds/epfl_starter_scaffold/epfl_pyramid_barebone/views/home.py
+++ solute/epfl/scaffolds/epfl_starter_scaffold/epfl_pyramid_barebone/views/home.py
@@ -9,4 +9,4 @@ class HomeRoot(epfl.components.CardinalLayout):
 
 @view_config(route_name='home')
 class HomePage(epfl.Page):
-    root_node = HomeRoot()
+    root_node = HomeRoot(constrained=True, node_list=[epfl.components.Box(title="Welcome to EPFL!")])
