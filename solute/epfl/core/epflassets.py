776ff8a300d23060c23bf40a1a41cec7f990d37b
Justus Wingert <juw@solute.de>
2015-01-20 10:59:40 +0100

    Implemented epfl_acl handle and epfl_has_role, epfl_has_permission convenience decorator.

--- solute/epfl/core/epflassets.py
+++ solute/epfl/core/epflassets.py
@@ -30,3 +30,91 @@ class ModelBase(object):
             output.append(tmp_data)
 
         return output
+
+
+from pyramid import security
+from functools import wraps
+
+
+def epfl_acl(permissions, default_allow=True, default_principal='system.Everyone', extend=False):
+    default_action = security.Deny
+    if default_allow:
+        default_action = security.Allow
+
+    if permissions is None:
+        permissions = []
+    elif type(permissions) is not list:
+        permissions = [permissions]
+
+    actions = {}
+    for permission in permissions:
+        action, principal = default_action, default_principal
+        if type(permission) is tuple and len(permission) == 2:
+            principal, permission = permission
+        elif type(permission) is tuple and len(permission) == 3:
+            action, principal, permission = permission
+            if action:
+                action = security.Allow
+            else:
+                action = security.Deny
+
+        if type(permission) is not list:
+            permission = [permission]
+
+        actions.setdefault(action, {}).setdefault(principal, permission)
+
+    acl = []
+    for action, _principals in actions.items():
+        for principal, _permission in _principals.items():
+            acl.append((action, principal, _permission))
+
+    def wrapper(cls):
+        _acl = acl
+        if extend:
+            #Retrieve any previous acl to extend if requested.
+            old_acl = getattr(cls, '__acl__', [])
+            old_acl.extend(_acl)
+            _acl = old_acl
+
+        setattr(cls, '__acl__', _acl)
+
+        return cls
+    return wrapper
+
+
+def epfl_has_permission(permission, fail_callback=None):
+    def wrapper(func):
+        @wraps(func)
+        def wrap(*args, **kwargs):
+            self = args[0]
+            request = self.request
+            if not request.has_permission(permission, self):
+                if fail_callback:
+                    return fail_callback(*args, **kwargs)
+                return
+            return func(*args, **kwargs)
+
+        return wrap
+
+    return wrapper
+
+
+def epfl_has_role(role, fail_callback=None):
+    class ACL(object):
+        def __init__(self, acl):
+            self.__acl__ = acl
+
+    def wrapper(func):
+        @wraps(func)
+        def wrap(*args, **kwargs):
+            self = args[0]
+            request = self.request
+            if not request.has_permission('has_role', ACL([(security.Allow, role, 'has_role')])):
+                if fail_callback:
+                    return fail_callback(*args, **kwargs)
+                return
+            return func(*args, **kwargs)
+
+        return wrap
+
+    return wrapper
