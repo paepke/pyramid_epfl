5945da7d083f2ed32530a304cbfee25a1c41563b
mast <mast@solute.de>
2015-01-23 15:44:29 +0100

    add route permission check

--- solute/epfl/components/link_list_layout/link_list_layout.py
+++ solute/epfl/components/link_list_layout/link_list_layout.py
@@ -6,6 +6,8 @@
 
 from solute.epfl.core import epflcomponentbase
 from solute.epfl.components import PaginatedListLayout
+from solute.epfl.core.epflassets import epfl_has_permission,epfl_acl
+
 
 class LinkListLayout(PaginatedListLayout):
     default_child_cls = epflcomponentbase.ComponentBase
@@ -22,10 +24,18 @@ class LinkListLayout(PaginatedListLayout):
     links = []
 
     def get_data(self, row_offset=None, row_limit=None, row_data=None):
-        for i, link in enumerate(self.links):
+        links = self.links[:]
+        for i, link in enumerate(links):
+            if not self.has_route_permission(link['url']):
+                links.remove(link)
+                continue
             link['id'] = i
-        return self.links
+        return links
 
+    def has_route_permission(self,route):
+        @epfl_has_permission(route)
+        def _check(self):
+            return True
 
-
+        return _check(self)
 
