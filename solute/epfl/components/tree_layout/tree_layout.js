23034488c260fa518ecc0dd4c14f92380518e288
Michael Hauck <michael.hauck@avenyou.com>
2015-01-26 10:41:48 +0100

    customizable node icons, adapted mouse pointer

--- solute/epfl/components/tree_layout/tree_layout.js
+++ solute/epfl/components/tree_layout/tree_layout.js
@@ -1,9 +1,9 @@
-$('[epflid=' + '{{ compo.cid }}] > i.fa-minus')
+$('[epflid=' + '{{ compo.cid }}] > .tree-label.expanded')
     .click(function () {
         epfl.send(epfl.make_component_event('{{ compo.cid }}', 'hide'));
     });
 
-$('[epflid=' + '{{ compo.cid }}] > i.fa-plus')
+$('[epflid=' + '{{ compo.cid }}] > .tree-label.collapsed')
     .click(function () {
         epfl.send(epfl.make_component_event('{{ compo.cid }}', 'show'));
     });
