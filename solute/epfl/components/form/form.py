da9e36ab4b468e7a6b4042f9e70d1f501a01764e
Michael Hauck <michael.hauck@avenyou.com>
2015-01-29 17:34:05 +0100

    form input fields can immediately fire change events if needed (default is false)

--- solute/epfl/components/form/form.py
+++ solute/epfl/components/form/form.py
@@ -5,37 +5,43 @@ from odict import odict
 
 class FormInputBase(epflcomponentbase.ComponentBase):
     asset_spec = "solute.epfl.components:form/static"
-    
 
     compo_state = ['label', 'name', 'value', 'validation_error']
     js_parts = ["form/input_base.js"]
-        
+
     js_name = ["input_base.js", "bootstrap3-typeahead.min.js"]
     css_name = ["input_base.css"]
 
-    label = None #: Optional label describing the input field.
-    name = None #: An element without a name cannot have a value.
-    value = None #: The actual value of the input element that is posted upon form submission.
-    default = None #: Default value that may be pre-set or pre-selected 
-    placeholder = None #: Placeholder text that can be displayed if supported by the input.
-    validation_error = '' #: Set during call of :func:`validate` with an error message if validation fails.
+    label = None  #: Optional label describing the input field.
+    name = None  #: An element without a name cannot have a value.
+    value = None  #: The actual value of the input element that is posted upon form submission.
+    default = None  #: Default value that may be pre-set or pre-selected
+    placeholder = None  #: Placeholder text that can be displayed if supported by the input.
+    #: Set during call of :func:`validate` with an error message if validation fails.
+    validation_error = ''
     validation_type = None
-    validation_helper = [] #: Subclasses can add their own validation helper lamdbas in order to extend validation logic.
-    mandatory = False #: Set to true if value has to be provided for this element in order to yield a valid form.
-    typeahead = False #: Set to true if typeahead should be provided by the input (if supported) 
-    submit_form_on_enter = False #: If true, underlying form is submitted upon enter key in this input 
-    input_focus = False #: Set focus on this input when component is displayed
-    
+    #: Subclasses can add their own validation helper lamdbas in order to extend validation logic.
+    validation_helper = []
+    #: Set to true if value has to be provided for this element in order to yield a valid form.
+    mandatory = False
+    typeahead = False  # : Set to true if typeahead should be provided by the input (if supported)
+    #: If true, underlying form is submitted upon enter key in this input
+    submit_form_on_enter = False
+    input_focus = False  # : Set focus on this input when component is displayed
+    #: Set to true if input change events should be fired immediately to the server.
+    #: Otherwise, change events are fired upon the next immediate epfl event.
+    fire_change_immediately = False
+
     def __init__(self, label=None, name=None, typeahead=False, default="", validation_type="",
                  **extra_params):
-        super(FormInputBase, self).__init__() 
+        super(FormInputBase, self).__init__()
 
     def is_numeric(self):
         return type(self.value) in [int, float]
 
     def init_transaction(self):
         super(FormInputBase, self).init_transaction()
-        
+
         if self.value is None and self.default is not None:
             self.value = self.default
 
@@ -106,11 +112,9 @@ class FormInputBase(epflcomponentbase.ComponentBase):
     def handle_change(self, value):
         self.value = value
 
-        
     def handle_typeahead(self, query):
         pass
         # TODO: How to return typeahead data to the caller?
-
 
 
 class Form(epflcomponentbase.ComponentContainerBase):
