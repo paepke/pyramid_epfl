d4542d559345848466699da63597e80db371df1b
Michael Hauck <michael.hauck@avenyou.com>
2015-01-29 10:27:09 +0100

    treelayout: improved layout for dynamic loading

--- solute/epfl/components/tree_layout/tree_layout.py
+++ solute/epfl/components/tree_layout/tree_layout.py
@@ -13,23 +13,24 @@ class TreeLayout(ComponentContainerBase):
     asset_spec = "solute.epfl.components:tree_layout/static"
     css_name = ["tree_layout.css"]
 
-    compo_state = ComponentContainerBase.compo_state + ['show_children']
+    compo_state = ComponentContainerBase.compo_state + ['show_children', 'tree_node_dict']
     js_parts = ComponentContainerBase.js_parts + ["tree_layout/tree_layout.js"]
 
     theme_path = ['tree_layout/theme']
 
     data_interface = {'id': None,
-                      'children': None,
+                      'children_ids': None,
                       'show_children': None,
                       'label': None}
 
     label = None
     id = None
-    children = []
+    children_ids = []
     show_children = False
     custom_node_icon_collapsed=None
     custom_node_icon_expanded=None
-    
+    tree_node_dict = {} #: This dict can store the data of the root nodes of this tree for caching: TreeModelBase respects this.
+     
     # folder icons
     #custom_node_icon_collapsed="fa-folder-o"
     #custom_node_icon_expanded="fa-folder-open-o"
