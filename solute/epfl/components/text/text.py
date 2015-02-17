0bd837acc7d983ad8ea7c5d5e062dab79768af29
mah <mah@solute.de>
2015-02-17 17:54:31 +0100

    text component: support label and vertical layout

--- solute/epfl/components/text/text.py
+++ solute/epfl/components/text/text.py
@@ -7,6 +7,13 @@ class Text(epflcomponentbase.ComponentBase):
     verbose = False
     tag = None
     tag_class = None
-    compo_state = ["value", "tag", "tag_class", "verbose"]
+    #: If set, display a label before the text. In this case, the :attr:`verbose` attribute is not needed and
+    # will be neglected, since verbose tags will be rendered anyway.
+    label = None
+    #: If set to True, the label will be rendered above the text instead of left before the text.
+    # This attribute is only regarded if the :attr:`label` attribute is set.
+    layout_vertical = False
+    compo_state = epflcomponentbase.ComponentBase.compo_state + ["value", "tag", "tag_class", "verbose", "label"]
 
-
+    def __init__(self, value=None, verbose=False, tag=None, tag_class=False, label=None, vertical_layout=False, **extra_params):
+        super(Text, self).__init__()
