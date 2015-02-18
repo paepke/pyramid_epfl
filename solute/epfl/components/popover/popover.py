4b0636ca471f95141f54bed33d615126883c3365
mah <mah@solute.de>
2015-02-18 15:36:11 +0100

    popover: enhanced display, support icon and labe attributes

--- solute/epfl/components/popover/popover.py
+++ solute/epfl/components/popover/popover.py
@@ -1,8 +1,11 @@
 from solute.epfl.core import epflcomponentbase
 
+
 class Popover(epflcomponentbase.ComponentBase):
+
     """
-    Popover dialog (a click triggered tooltip) using bootstrap popover
+    Popover dialog (a click triggered tooltip) using bootstrap popover.
+    The popover dialog is displayed when clicking a button.
 
     Example: http://getbootstrap.com/javascript/#popovers
 
@@ -13,11 +16,23 @@ class Popover(epflcomponentbase.ComponentBase):
     js_parts = epflcomponentbase.ComponentBase.js_parts + ["popover/popover.js"]
     asset_spec = "solute.epfl.components:popover/static"
 
+    css_name = ["popover.css"]
     js_name = ["popover.js"]
 
     compo_config = []
-    compo_state = ["text", "title","position"]
+    compo_state = ["text", "title", "position", "label", "icon"]
 
     text = []
-    title = None #: if title is none it is not displayed
-    position = "top" #: posible positions are top,left,right,bottom
+    title = None  # : if title is none it is not displayed
+    position = "top"  # : posible positions are top,left,right,bottom
+    #: An optional font-awesome icon that should be displayed on the button.
+    # Either the :attr:`icon` or the :attr:`label` have to be defined in order to yield a reasonable button.
+    icon = None
+    #: An optional label that should be displayed on the button.
+    # Either the :attr:`icon` or the :attr:`label` have to be defined in order to yield a reasonable button.
+    label = None
+    
+    #def delete_component(self):
+    #    self.add_js_response('epfl.destroy_component("{cid}");'.format(cid=self.cid))
+    #    epflcomponentbase.ComponentBase.delete_component(self)
+        
--- solute/epfl/components/popover/static/popover.js
+++ solute/epfl/components/popover/static/popover.js
@@ -1,6 +1,9 @@
 epfl.Popover = function (cid, params) {
     epfl.ComponentBase.call(this, cid, params);
     $('#'+cid+"_popover").popover();
+    $('#'+cid+"_popover").on("remove", function () {
+    	$('#'+cid+"_popover").popover('destroy');
+	})
 };
 
 epfl.Popover.inherits_from(epfl.ComponentBase);
