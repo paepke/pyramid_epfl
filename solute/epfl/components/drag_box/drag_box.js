36a5a75c992d2a8b6eb04ccb0a7b10e0efbc1f0a
mah <mah@solute.de>
2015-03-18 16:49:31 +0100

    fix correct initialization of non-primitive component attributes

--- solute/epfl/components/drag_box/drag_box.js
+++ solute/epfl/components/drag_box/drag_box.js
@@ -1,4 +1,4 @@
 epfl.init_component("{{compo.cid}}", "DragBoxComponent", {});
-{% if compo.disable_drag is None or (not compo.disable_drag) %}
-epfl.make_compo_dragable("{{ compo.cid }}", {"keep_orig_in_place": {{ "false" if compo.keep_orig_in_place is None else compo.keep_orig_in_place|format_bool }}});
+{% if (not compo.disable_drag) %}
+epfl.make_compo_dragable("{{ compo.cid }}", {"keep_orig_in_place": {{ "false" if compo.keep_orig_in_place is none else compo.keep_orig_in_place|format_bool }}});
 {% endif %}
