7aa070ae3178439006325bd40458f157e9acac5c
mah <mah@solute.de>
2015-03-18 14:24:27 +0100

    modal box component: added docs

--- solute/epfl/components/box/box.py
+++ solute/epfl/components/box/box.py
@@ -4,7 +4,8 @@
 
 """
 
-import types, copy
+import types
+import copy
 
 from pyramid import security
 
@@ -23,25 +24,33 @@ class Box(epflcomponentbase.ComponentContainerBase):
     js_parts = epflcomponentbase.ComponentContainerBase.js_parts + ["box/box.js"]
     css_name = ["box.css"]
     js_name = ["box.js"]
-    
-    title = None #: The title of a box. If it is set, a heading panel is rendered that contains the title.
 
-    auto_visibility = True # the visibility of the box depends on the visibility of the containing template-elements
-                           # if none of them (compos or form-fields) are visible the box to is not visible
-                           # else the box is visible
+    # : The title of a box. If it is set, a heading panel is rendered that contains the title.
+    title = None
 
+    #: the visibility of the box depends on the visibility of the containing template-elements
+    #: if none of them (compos or form-fields) are visible the box to is not visible
+    #: else the box is visible
+    auto_visibility = True
+
+    #: Set to true if box should not be displayed inside its parent component, but rendered as modal hover box on the overall page.
     hover_box = False
+    #: Indicates whether a hover box component should be deleted upon close or just be hidden.
     hover_box_remove_on_close = True
+    #: Indicates of a border should be drawn around the box.
     box_shown = True
+    #: Indicates if the title of the box should be shown. Sometimes, you want to specify a title but not show it inside the box.
+    #: For example, a box inside a :class:`solute.epfl.components.tabs_layout.tabs_layout.TabsLayout` component.
     show_title = True
+    #: Indicates whether a box can be closed by clicking on a special 'close' button
     is_removable = False
-    
+
     def handle_removed(self):
         self.delete_component()
-    
+
     def handle_hide(self):
-        self.visible=False
-    
+        self.visible = False
+
     def after_event_handling(self):
         super(Box, self).after_event_handling()
 
@@ -49,12 +58,12 @@ class Box(epflcomponentbase.ComponentContainerBase):
         if self.auto_visibility:
 
             if not self.components:
-                return True # No subelements -> I am visible!
+                return True  # No subelements -> I am visible!
 
             some_visible = False
             for el in self.components:
                 if el.is_visible():
-                    some_visible = True # at least one subelement is visible -> I am visible!
+                    some_visible = True  # at least one subelement is visible -> I am visible!
                     break
 
             # Automatic redraw-handling based on the old visibility state:
@@ -69,18 +78,25 @@ class Box(epflcomponentbase.ComponentContainerBase):
 
 
 class ModalBox(Box):
-    hover_box = True
-    visible = False
-    auto_visibility = False
-    is_removable = True
-    hover_box_remove_on_close = False
+    hover_box = True  #: see :attr:`Box.hover_box`
+    visible = False  #: see :attr:`Box.hover_box`
+    auto_visibility = False  #: see :attr:`Box.hover_box`
+    is_removable = True  #: see :attr:`Box.hover_box`
+    hover_box_remove_on_close = False  #: see :attr:`Box.hover_box`
+    #: Used to specify the width of the modal. The width is given in percentage of the full page width.
     hover_box_width = 50
-    
+
     def open(self):
+        """
+        Open and display the modal box.
+        """
         self.visible = True
         self.redraw()
         self.add_ajax_response("$('body').css({ overflow: 'hidden' });")
-        
+
     def handle_hide(self):
+        """
+        Called when modal box is closed.
+        """
         Box.handle_hide(self)
         self.add_ajax_response("$('body').css({ overflow: 'inherit' });")
