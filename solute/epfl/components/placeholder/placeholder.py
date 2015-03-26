23c56b3dac778bbf39a6b4855d96b0eb3f7c296a
mah <mah@solute.de>
2015-03-26 15:33:25 +0100

    placeholder: remove type from compo state

--- solute/epfl/components/placeholder/placeholder.py
+++ solute/epfl/components/placeholder/placeholder.py
@@ -4,6 +4,3 @@ from solute.epfl.core import epflcomponentbase
 class Placeholder(epflcomponentbase.ComponentBase):
     template_name = "placeholder/placeholder.html"
     type = None
-    compo_state = ["type"]
-
-
