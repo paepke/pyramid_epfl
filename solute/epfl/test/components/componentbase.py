0eda21082d435f5b8b7276b0ec0cfaf99aa1323b
Justus Wingert <juw@solute.de>
2015-04-01 17:26:04 +0200

    Added descriptions for all asserts.

--- solute/epfl/test/components/componentbase.py
+++ solute/epfl/test/components/componentbase.py
@@ -90,21 +90,30 @@ class ComponentBaseTest(unittest.TestCase):
         self.assert_component_base_properties(page.tested_component, compo_info)
 
     def assert_component_base_properties(self, component, compo_info):
-        assert isinstance(component, epfl.core.epflcomponentbase.ComponentBase)
+        assert isinstance(component, epfl.core.epflcomponentbase.ComponentBase),\
+            "{compo} is not a {base_compo} instance.".format(
+                compo=component, base_compo=epfl.core.epflcomponentbase.ComponentBase)
 
-        assert component.slot == compo_info['slot']
-        assert component.cid == compo_info['cid']
-        assert component.__unbound_component__.__getstate__() == compo_info['class']
+        assert component.slot == compo_info['slot'], "slot attribute differs in transaction and instance"
+        assert component.cid == compo_info['cid'], "cid attribute differs in transaction and instance"
+        assert component.__unbound_component__.__getstate__() == compo_info['class'],\
+            "class attribute differs in transaction and instance"
 
-        assert component._ComponentBase__config == compo_info['config']
+        assert component._ComponentBase__config == compo_info['config'], \
+            "config not stored correctly in transaction"
 
         for name in component.combined_compo_state:
             attr_value = getattr(component, name)
             setattr(component, name, attr_value)
-            assert compo_info['compo_state'][name] == attr_value
+            assert compo_info['compo_state'][name] == attr_value,\
+                "compo_state not stored correctly for {name}. Should be {attr_value} actually is {state_value}".format(
+                    name=name,
+                    attr_value=attr_value,
+                    state_value=compo_info['compo_state'][name]
+                )
 
     def assert_rendered_html_base_parameters(self, html, cid):
-        assert html.count('epflid="{cid}"'.format(cid=cid)) == 1
+        assert html.count('epflid="{cid}"'.format(cid=cid)) == 1, "element {cid} is missing".format(cid=cid)
 
     def assert_rendered_js_base_parameters(self, html):
         pass
@@ -126,40 +135,63 @@ class ComponentBaseTest(unittest.TestCase):
                 static_path = package_path + '/static'
                 static_js_file_path = static_path + '/{compo_name}.js'.format(compo_name=compo_name.lower())
 
-                assert file_path.endswith(compo_name.lower() + '.py')
-                assert package_path.endswith(compo_name.lower())
-                assert os.path.exists(static_path)
+                assert file_path.endswith(compo_name.lower() + '.py'),\
+                    "{compo_name} is missing primary python file.".format(compo_name=compo_name)
+                assert package_path.endswith(compo_name.lower()),\
+                    "{compo_name} has a malformed package path." \
+                    " Should end with {compo_name_lower} actually is {package_path}".format(
+                        compo_name=compo_name,
+                        compo_name_lower=compo_name.lower(),
+                        package_path=package_path,
+                    )
+                assert os.path.exists(static_path), "{compo_name} is missing static folder.".format(
+                    compo_name=compo_name)
 
                 if os.path.exists(js_file_path):
                     js_file = file(js_file_path).read()
-                    assert js_file.startswith('epfl.init_component("{{ compo.cid }}"')
-                    assert js_file.startswith('epfl.init_component("{{ compo.cid }}", "%s", {' % compo_name)
+                    assert js_file.startswith('epfl.init_component("{{ compo.cid }}"'),\
+                        '{compo_name} has dynamic js missing \'epfl.init_component("{{{{ compo.cid }}}}"\''.format(
+                            compo_name=compo_name)
+                    assert js_file.startswith('epfl.init_component("{{ compo.cid }}", "%s", {' % compo_name),\
+                        '{compo_name} has dynamic js missing ' \
+                        '\'epfl.init_component("{{{{ compo.cid }}}}", "{compo_name}"\''.format(
+                            compo_name=compo_name)
                 if os.path.exists(static_js_file_path):
                     js_file = file(static_js_file_path).read()
 
                     r = re.compile("epfl\.{compo_name}\.inherits_from\(epfl\.([A-Za-z]*)\);".format(
                         compo_name=compo_name))
                     result = r.findall(js_file)
-                    assert len(result) == 1
-                    assert js_file.count("epfl.{base_compo}.call(this, cid, params);".format(base_compo=result[0])) == 1
+                    assert len(result) == 1,\
+                        "{compo_name} is missing inheritance calls in static js.".format(compo_name=compo_name)
+                    assert js_file.count("epfl.{base_compo}.call(this, cid, params);".format(base_compo=result[0])) == 1,\
+                        "{compo_name} is missing inherited initiation call in static js. " \
+                        "'epfl.{base_compo}.call(this, cid, params);' not found".format(
+                            compo_name=compo_name, base_compo=result[0])
 
                     assert js_file.startswith("epfl.{compo_name} = function(cid, params) ".format(
-                        compo_name=compo_name))
+                        compo_name=compo_name)),\
+                        "{compo_name} is not correctly created in static js."
                     assert js_file.count("epfl.{compo_name}.inherits_from(epfl.".format(
-                        compo_name=compo_name)) == 1
+                        compo_name=compo_name)) == 1,\
+                        "{compo_name} is not correctly inheriting in static js."
 
         init_func = component.__init__
         init_docs = init_func.__doc__
         init_code = init_func.func_code
-        assert init_docs
+        assert init_docs, "{compo_name} __init__ method has no doc string.".format(compo_name=compo_name)
         if component not in [epfl.core.epflcomponentbase.ComponentBase,
                              epfl.core.epflcomponentbase.ComponentContainerBase]:
-            assert 'page' in init_code.co_varnames  # This parameter is not set in the ComponentBase, so it's only set
-            # for custom __init__ methods that are required for all components.
+            # This parameter is not set in the ComponentBase, so it's only set
+            # for custom __init__ methods that are required for all components
+            assert 'page' in init_code.co_varnames,\
+                "{compo_name} __init__ method is not correctly setup. " \
+                "(Missing page parameter, or not overwritten.)".format(compo_name=compo_name)
         for var in init_code.co_varnames:
             if var in ['self', 'page', 'args', 'kwargs', 'extra_params']:
                 continue
-            assert ":param {var}:".format(var=var) in init_docs
+            assert ":param {var}:".format(var=var) in init_docs,\
+                "{compo_name} __init__ method is missing docs for {param}.".format(compo_name=compo_name, param=var)
 
 
 class ComponentContainerBaseTest(ComponentBaseTest):
@@ -189,8 +221,11 @@ class ComponentContainerBaseTest(ComponentBaseTest):
 
         test_node_0.delete_component()
 
-        assert root_node.components[0] == test_node_1
-        assert len(root_node.components) == 1
+        assert root_node.components[0] == test_node_1,\
+            "{component} failed during deletion of child. (Wrong child found in position 1)".format(
+                component=self.component)
+        assert len(root_node.components) == 1,\
+            "{component} failed during deletion of child. (Too many children left)".format(component=self.component)
 
     def test_rendering_sub_components(self):
         page = self.page
