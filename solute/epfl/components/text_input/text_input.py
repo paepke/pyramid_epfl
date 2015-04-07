600261109dd588121f3d69fbc771a7d6d8803414
mah <mah@solute.de>
2015-04-07 12:43:50 +0200

    use consistent search input, support layover icon for text_input component

--- solute/epfl/components/text_input/text_input.py
+++ solute/epfl/components/text_input/text_input.py
@@ -2,26 +2,30 @@ from solute.epfl.components.form.form import FormInputBase
 
 
 class TextInput(FormInputBase):
+
     """
     A form text input.
-    
+
     Typically, this component is used in a form:
-    
+
     .. code:: python
-        
+
         form = Form(node_list=[TextInput(label="User name:", name="username")])
-    
+
     """
 
     js_parts = FormInputBase.js_parts[:]
     js_parts.extend(['text_input/text_input.js'])
 
-    js_name = FormInputBase.js_name + [("solute.epfl.components:text_input/static", "text_input.js")]
-    css_name = FormInputBase.css_name + [("solute.epfl.components:text_input/static", "text_input.css")]
-    
+    js_name = FormInputBase.js_name + \
+        [("solute.epfl.components:text_input/static", "text_input.js")]
+    css_name = FormInputBase.css_name + \
+        [("solute.epfl.components:text_input/static", "text_input.css")]
 
     template_name = "text_input/text_input.html"
-    
+
     validation_type = 'text'
 
     password = False
+    #: optional font-awesome icon to be rendered as a layover icon above the input field (aligned to the right)
+    layover_icon = None
