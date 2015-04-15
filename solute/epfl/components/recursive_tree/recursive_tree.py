4685228d2b6e0642cadb6b9fa07478c152922fe7
Justus Wingert <juw@solute.de>
2015-04-15 14:09:35 +0200

    Minor test updates, added RecursiveTree test.

--- solute/epfl/components/recursive_tree/recursive_tree.py
+++ solute/epfl/components/recursive_tree/recursive_tree.py
@@ -10,7 +10,7 @@ class RecursiveTree(epflcomponentbase.ComponentContainerBase):
     js_name = ['recursive_tree.js']
 
     compo_state = epflcomponentbase.ComponentContainerBase.compo_state + [
-        'icon_open', 'icon_close', 'label', 'default_child_cls', 'get_data', 'show_children'
+        'icon_open', 'icon_close', 'label', 'default_child_cls', 'get_data', 'show_children', 'data_interface'
     ]
     icon_open = None
     icon_close = None
@@ -22,14 +22,32 @@ class RecursiveTree(epflcomponentbase.ComponentContainerBase):
 
     show_children = False
 
+    def __init__(self, page, cid, show_children=False, get_data=None, default_child_cls=None, data_interface=None,
+                 *args, **kwargs):
+        """Simple tree component.
+
+        :param show_children: Load and show any potential children that would be returned by polling get_data.
+        :param get_data: List of get_data sources. First entry is used for first level, second for second, and so on.
+        :param default_child_cls: Default component to be used to initialize children.
+        :param data_interface: Data interface to translate the results from get_data polling. If a list is given it will
+                               be used for the according levels given in the get_data list. Else this is used on all
+                               children.
+        """
+        super(RecursiveTree, self).__init__(page, cid, show_children, get_data, default_child_cls, *args, **kwargs)
+
     def init_struct(self):
         if self.default_child_cls is None:
-            self.default_child_cls = RecursiveTree
+            self.default_child_cls = RecursiveTree(data_interface=self.data_interface)
         if type(self.get_data) is list:
             data = self.get_data[:]
             self.get_data = data.pop(0)
             if len(data) > 0:
-                self.default_child_cls = self.default_child_cls(get_data=data)
+                if type(self.data_interface) is list:
+                    data_interface = self.data_interface[:]
+                    self.data_interface = data_interface.pop(0)
+                    self.default_child_cls = self.default_child_cls(get_data=data, data_interface=data_interface)
+                else:
+                    self.default_child_cls = self.default_child_cls(get_data=data)
 
     def handle_click_label(self):
         pass
--- solute/epfl/test/components/generic.py
+++ solute/epfl/test/components/generic.py
@@ -1,7 +1,4 @@
-import unittest
-from pyramid import testing
 import solute.epfl as epfl
-import pyramid_jinja2
 import inspect
 
 from componentbase import ComponentBaseTest, ComponentContainerBaseTest
