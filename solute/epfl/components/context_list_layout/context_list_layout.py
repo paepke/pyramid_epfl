09793050b1305114b28dd43d91b2e8117415f0a9
svh <svh@solute.de>
2015-04-23 13:46:42 +0200

    Added init-function, documentation and a default menu.

--- solute/epfl/components/context_list_layout/context_list_layout.py
+++ solute/epfl/components/context_list_layout/context_list_layout.py
@@ -51,6 +51,24 @@ class ContextListLayout(PaginatedListLayout):
 
     """
 
+    def __init__(self, page, cid, show_search=True, get_data=None, default_child_cls=None, data_interface=None, *args, **kwargs):
+        """ContextListLayout Component
+
+        :param get_data: List of get_data sources. First entry is used for first level, second for second, and so on.
+        :param show_search:
+        :param default_child_cls: Default component to be used to initialize children.
+        :param data_interface: Data interface to translate the results from get_data polling. If a list is given it will
+                               be used for the according levels given in the get_data list. Else this is used on all
+                               children.
+        """
+        kwargs.update({
+            'show_search': show_search,
+            'get_data': get_data,
+            'default_child_cls': default_child_cls,
+            'data_interface': data_interface
+        })
+        super(ContextListLayout, self).__init__(page, cid, *args, **kwargs)
+
     theme_path = {'default': ['context_list_layout/theme'],
                   'container': ['pretty_list_layout/theme'],
                   # context layout embraces paginated layout template  for before and after
@@ -71,3 +89,12 @@ class ContextListLayout(PaginatedListLayout):
     css_name = PaginatedListLayout.css_name + [("solute.epfl.components:context_list_layout/static", "context_list_layout.css")]
 
     data_interface = {'id': None, 'data': None, 'menu': None}
+
+    default_menu = [{'name':u"Delete", 'event':"delete", 'type':"link"},
+                    {'name':"Rename", 'event':"rename", 'type':"link"}]
+
+    def handle_delete(self, entry_id, data):
+        pass
+
+    def handle_rename(self, entry_id, data):
+        pass
