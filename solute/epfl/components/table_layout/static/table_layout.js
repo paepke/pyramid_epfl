cc4be54b33aa27b4e4c1d49104b224d844b7db89
Justus Wingert <juw@solute.de>
2015-05-12 09:54:18 +0200

    Bugfix: Table component had some js and css errors that prevented showing it on dynamic rendering.

--- solute/epfl/components/table_layout/static/table_layout.js
+++ solute/epfl/components/table_layout/static/table_layout.js
@@ -2,9 +2,17 @@ epfl.TableLayout = function(cid, params) {
     epfl.ComponentBase.call(this, cid, params);
 	var compo = this;
     var selector = "#" + cid;
-    var panel = $(selector + '_panel');
-	var panel_height = panel.height();
-    $(selector).fixedHeaderTable({autoShow: true, height: panel_height - 80, themeClass: 'deal-table'});
+
+    compo.enable_fixed_header_table = function () {
+        var panel = $(selector);
+        if (!panel.is(':visible')) {
+            return setTimeout(compo.enable_fixed_header_table, 0);
+        }
+
+        $('#table_' + cid).fixedHeaderTable({autoShow: true, height: panel.height() - 80});
+    };
+
+    setTimeout(compo.enable_fixed_header_table, 0);
 }; 
 epfl.TableLayout.inherits_from(epfl.ComponentBase);
 
