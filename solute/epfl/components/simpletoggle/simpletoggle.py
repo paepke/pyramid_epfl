b5e5703f6ea01ebc98ca6e6685c2917084b2d8d7
Marc Stipcevic <mast@solute.de>
2015-06-01 10:00:32 +0200

    SimpleToggle: icon, icon_size and icon_color could now be configured

--- solute/epfl/components/simpletoggle/simpletoggle.py
+++ solute/epfl/components/simpletoggle/simpletoggle.py
@@ -2,7 +2,6 @@ from solute.epfl.components.form.form import FormInputBase
 
 
 class SimpleToggle(FormInputBase):
-
     """
     A form checkbox styled as a simple on/off toggle.
     Compared to the Toggle component, this component does not display any texts on the toggle.
@@ -20,13 +19,22 @@ class SimpleToggle(FormInputBase):
     validation_type = 'bool'  #: Form validation selector.
 
     js_name = FormInputBase.js_name + \
-        [("solute.epfl.components:simpletoggle/static", "simpletoggle.js")]
+              [("solute.epfl.components:simpletoggle/static", "simpletoggle.js")]
     css_name = FormInputBase.css_name + \
-        [("solute.epfl.components:simpletoggle/static", "simpletoggle.css")]
+               [("solute.epfl.components:simpletoggle/static", "simpletoggle.css")]
 
     default = False  #: The default value of the toggle.
 
     js_parts = FormInputBase.js_parts + ['simpletoggle/simpletoggle.js']
+
+    compo_state = FormInputBase.compo_state + ["enabled_icon","disabled_icon","enabled_icon_size","disabled_icon_size","enabled_icon_color","disabled_icon_color"]
+
+    enabled_icon = None #: font-awesome icon to be renderd if value == True
+    disabled_icon = None #: font-awesome icon to be renderd if value == False
+    enabled_icon_size = None #: font-awesome icon size if value == True example lg,2x,3x etc
+    disabled_icon_size = None #: font-awesome icon size if value == False  lg,2x,3x etc
+    enabled_icon_color = None #: bootstrap color if value == True example: primary default warning etc
+    disabled_icon_color = None #: bootstrap color if value == False example: primary default warning etc
 
     def __init__(self, page, cid,
                  label=None,
@@ -42,6 +50,12 @@ class SimpleToggle(FormInputBase):
                  layout_vertical=False,
                  label_style=None,
                  input_style=None,
+                 enabled_icon="toggle-on",
+                 disabled_icon="toggle-off",
+                 enabled_icon_size="lg",
+                 disabled_icon_size="lg",
+                 enabled_icon_color="primary",
+                 disabled_icon_color="default",
                  **extra_params):
         """
         Form simple toggle Component
@@ -52,9 +66,9 @@ class SimpleToggle(FormInputBase):
         :param default: Default boolean value that may be pre-set
         :param readonly: Set to true if input cannot be changed and is displayed in readonly mode
         :param validation_error: Set during call of :func:`validate` with an error message if validation fails
-        :param mandatory: Set to true if value has to be provided for this element in order to yield a valid form 
+        :param mandatory: Set to true if value has to be provided for this element in order to yield a valid form
         :param fire_change_immediately: Set to true if input change events should be fired immediately to the server. Otherwise, change events are fired upon the next immediate epfl event
-        :param compo_col: Set the width of the complete input component (default: max: 12) 
+        :param compo_col: Set the width of the complete input component (default: max: 12)
         :param label_col: Set the width of the complete input component (default: 2)
         :param layout_vertical: Set to true if label should be displayed on top of the input and not on the left before it
         :param label_style: Can be used to add additional css styles for the label
@@ -64,6 +78,13 @@ class SimpleToggle(FormInputBase):
         :param show_count: Set to true to show a input counter right to the field. Requires a max_length to be set
         :param password: Set to true if input field should be used as a password field
         :param layover_icon: Optional font-awesome icon to be rendered as a layover icon above the input field (aligned to the right)
+        :param enabled_icon: font-awesome icon to be renderd if value == True
+        :param disabled_icon: font-awesome icon to be renderd if value == False
+        :param enabled_icon_size: font-awesome icon size if value == True example lg,2x,3x etc
+        :param disabled_icon_size: font-awesome icon size if value == False  lg,2x,3x etc
+        :param enabled_icon_color: bootstrap color if value == True example: primary default warning etc
+        :param disabled_icon_color: bootstrap color if value == False example: primary default warning etc
+        :return:
         """
         super(SimpleToggle, self).__init__(page, cid,
                                            label=label,
@@ -79,4 +100,10 @@ class SimpleToggle(FormInputBase):
                                            layout_vertical=layout_vertical,
                                            label_style=label_style,
                                            input_style=input_style,
+                                           enabled_icon=enabled_icon,
+                                           disabled_icon=disabled_icon,
+                                           enabled_icon_size=enabled_icon_size,
+                                           disabled_icon_size=disabled_icon_size,
+                                           enabled_icon_color=enabled_icon_color,
+                                           disabled_icon_color=disabled_icon_color,
                                            **extra_params)
