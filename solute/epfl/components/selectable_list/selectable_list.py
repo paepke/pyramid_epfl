acb9e1dcab338db07d35127b89fdc73d8dfe5e34
Marc Stipcevic <mast@solute.de>
2015-06-02 09:17:28 +0200

    Implement search for selectable list and text list

--- solute/epfl/components/selectable_list/selectable_list.py
+++ solute/epfl/components/selectable_list/selectable_list.py
@@ -23,6 +23,10 @@ class SelectableList(PaginatedListLayout):
 
     js_name = PaginatedListLayout.js_name + [('solute.epfl.components:selectable_list/static', 'selectable_list.js')]
 
+    compo_state = PaginatedListLayout.compo_state + ["search_text"]
+
+    search_text = None
+
     def __init__(self,page,cid, data_interface=None, *args, **extra_params):
         """
         Selectable List is a MultiSelect Component, multiple values can be selected
@@ -39,3 +43,9 @@ class SelectableList(PaginatedListLayout):
         :return: a list with selected compontents
         """
         return [compo for compo in self.components if compo.selected]
+
+    def handle_set_row(self, row_offset, row_limit, row_data=None):
+        if row_data is not None:
+            self.search_text = row_data.get("search")
+        self.update_children()
+        self.redraw()
--- solute/epfl/components/text_list/text_list.py
+++ solute/epfl/components/text_list/text_list.py
@@ -14,6 +14,10 @@ class TextList(PaginatedListLayout):
     theme_path = {'default': PaginatedListLayout.theme_path,
                   'row': ['text_list/theme']}
 
+    compo_state = PaginatedListLayout.compo_state + ["search_text"]
+
+    search_text = None
+
 
     def __init__(self,page,cid, data_interface=None, *args, **extra_params):
         """
@@ -21,3 +25,9 @@ class TextList(PaginatedListLayout):
         :param data_interface: data interface for child class needs id and text
         """
         super(TextList, self).__init__(page,cid,data_interface=data_interface, *args, **extra_params)
+
+    def handle_set_row(self, row_offset, row_limit, row_data=None):
+        if row_data is not None:
+            self.search_text = row_data.get("search")
+        self.update_children()
+        self.redraw()
