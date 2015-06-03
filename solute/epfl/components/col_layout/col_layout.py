0453d978aa79da0ff4dc8e065117cd417f8463fb
mah <mah@solute.de>
2015-06-03 10:12:22 +0200

    ColLayout: Switched to new style, updated docs, make tests pass, added support for col_class attribute on child components

--- solute/epfl/components/col_layout/col_layout.py
+++ solute/epfl/components/col_layout/col_layout.py
@@ -21,8 +21,14 @@ class ColLayout(epflcomponentbase.ComponentContainerBase):
         )
 
 
-    Every child component is required to have the cols attribute indicating the number of cols it
-    should use. The cols attribute must a number between 1 and 12.
+    Every child component is should have the cols attribute indicating the number of cols it
+    should use. The cols attribute must a number between 1 and 12. If it is missing, 12 is used.
+    
+    To create responsive layouts (see http://getbootstrap.com/css/#grid), col classes can be used for 
+    a column. For this, a child components can have a col_class attribute ("xs", "sm", "md", "lg")
+    indicating the col class (e.g. "col-sm-12", "col-md-6", etc.) for the cols. If the col_class attribute
+    is missing for a child component, "md" is used. 
+    
     A child component can also optionally have the align attribute ("left", "right" or "center"),
     indicating that it should be aligned in a certain way.
 
@@ -30,9 +36,14 @@ class ColLayout(epflcomponentbase.ComponentContainerBase):
 
     asset_spec = "solute.epfl.components:col_layout/static"
     css_name = ["col_layout.css"]
+    js_name = ["col_layout.js"]
+    js_parts = []
 
     template_name = "col_layout/col_layout.html"
     vertical_center = False  #: If set to true, child components are centered vertically.
+
+    new_style_compo = True
+    compo_js_name = 'ColLayout'
 
     def __init__(self, page, cid, vertical_center=False, **extra_params):
         """
