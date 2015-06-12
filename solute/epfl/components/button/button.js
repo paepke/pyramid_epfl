ebc663d7b6ab0c3becbee9117444d2cf02744aa8
mah <mah@solute.de>
2015-06-12 13:58:18 +0200

    button component: added option to disable button directly on click

--- solute/epfl/components/button/button.js
+++ solute/epfl/components/button/button.js
@@ -1,4 +1,5 @@
 epfl.init_component("{{ compo.cid }}", "Button", {"event_name" : "{{ compo.event_name }}",
-														   "event_target" : "{{ compo.event_target }}",
-														   "confirm_first": {{ compo.confirm_first|format_bool }},
-														   "confirm_message": "{{ compo.confirm_message }}" });
+                                                  "event_target" : "{{ compo.event_target }}",
+                                                  "confirm_first": {{ compo.confirm_first|format_bool }},
+                                                  "confirm_message": "{{ compo.confirm_message }}",
+                                                  "disable_on_click": {{ compo.disable_on_click|format_bool }},});
