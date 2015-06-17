b257a22f346e715b98ee1a7178182e5197600283
Justus Wingert <juw@solute.de>
2015-06-17 12:23:10 +0200

    Added icon to Link.

--- solute/epfl/components/link/link.py
+++ solute/epfl/components/link/link.py
@@ -13,11 +13,12 @@ class Link(ComponentBase):
     
     url = '#'  #: The url this link points to. Used as src attribute of the A-Tag.
     name = 'Please change me!'  #: The name displayed for this link.
+    icon = None  #: The icon to be displayed in front of the text.
 
     new_style_compo = True
     compo_js_name = 'Link'
 
-    def __init__(self, page, cid, url=None, name=None, **extra_params):
+    def __init__(self, page, cid, url=None, name=None, icon=None, **extra_params):
         """Simple Link component.
 
         Usage:
@@ -30,6 +31,7 @@ class Link(ComponentBase):
 
         :param url: The url this link points to. Used as src attribute of the A-Tag.
         :param name: The name displayed for this link.
+        :param icon: The icon to be displayed in front of the text.
         """
         super(Link, self).__init__(page, cid, url=url, name=name, **extra_params)
 
