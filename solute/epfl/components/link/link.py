d975dc5590e544e620629c03359df64a9728cad5
Justus Wingert <juw@solute.de>
2015-06-22 09:49:39 +0200

    Tweaked the link component for more functionality.

--- solute/epfl/components/link/link.py
+++ solute/epfl/components/link/link.py
@@ -8,17 +8,20 @@ class Link(ComponentBase):
     asset_spec = "solute.epfl.components:link/static"
     template_name = "link/link.html"
     js_name = ["link.js"]
+    css_name = ["link.css"]
 
     compo_state = ["url", "name"]
     
     url = '#'  #: The url this link points to. Used as src attribute of the A-Tag.
     name = 'Please change me!'  #: The name displayed for this link.
     icon = None  #: The icon to be displayed in front of the text.
+    breadcrumb = False  #: Display the link as a breadcrumb.
+    tile = False  #: Display the link as a rectangular tile.
 
     new_style_compo = True
     compo_js_name = 'Link'
 
-    def __init__(self, page, cid, url=None, name=None, icon=None, **extra_params):
+    def __init__(self, page, cid, url=None, name=None, icon=None, breadcrumb=None, tile=None, **extra_params):
         """Simple Link component.
 
         Usage:
@@ -32,6 +35,8 @@ class Link(ComponentBase):
         :param url: The url this link points to. Used as src attribute of the A-Tag.
         :param name: The name displayed for this link.
         :param icon: The icon to be displayed in front of the text.
+        :param breadcrumb: Display the link as a breadcrumb.
+        :param tile: Display the link as a rectangular tile.
         """
         super(Link, self).__init__(page, cid, url=url, name=name, **extra_params)
 
