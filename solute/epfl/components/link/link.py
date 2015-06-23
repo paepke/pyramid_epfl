1f45717e3687e4c62e0ab7c714375a0acb47d3ba
mah <mah@solute.de>
2015-06-23 17:50:36 +0200

    link & breadcrump: reverted route attribute as it caused setting the url attribute to fail

--- solute/epfl/components/link/link.py
+++ solute/epfl/components/link/link.py
@@ -11,8 +11,8 @@ class Link(ComponentBase):
     css_name = ["link.css"]
 
     compo_state = ["url", "name"]
-    
-    route = None  #: The route this link points to. Used to look up the url for the src attribute of the A-Tag.
+
+    url = '#'  #: The url this link points to. Used as src attribute of the A-Tag.
     name = 'Please change me!'  #: The name displayed for this link.
     icon = None  #: The icon to be displayed in front of the text.
     breadcrumb = False  #: Display the link as a breadcrumb.
@@ -21,7 +21,7 @@ class Link(ComponentBase):
     new_style_compo = True
     compo_js_name = 'Link'
 
-    def __init__(self, page, cid, url=None, route=None, name=None, icon=None, breadcrumb=None, tile=None, **extra_params):
+    def __init__(self, page, cid, url=None, name=None, icon=None, breadcrumb=None, tile=None, **extra_params):
         """Simple Link component.
 
         Usage:
@@ -32,21 +32,13 @@ class Link(ComponentBase):
                 url="http://www.billiger.de"
             )
 
-        :param url: The url this link points to. Used as src attribute of the A-Tag. If present route will be ignored.
-        :param route: The route this link points to. Used to look up the url for the src attribute of the A-Tag.
+        :param url: The url this link points to. Used as src attribute of the A-Tag.
         :param name: The name displayed for this link.
         :param icon: The icon to be displayed in front of the text.
         :param breadcrumb: Display the link as a breadcrumb.
         :param tile: Display the link as a rectangular tile.
         """
-        super(Link, self).__init__(page, cid, url=url, route=route, name=name, icon=icon, breadcrumb=breadcrumb,
-                                   tile=tile, **extra_params)
-
-    @property
-    def url(self):
-        if self.route is None:
-            return
-        return self.page.get_route_path(self.route)
+        super(Link, self).__init__(page, cid, url=url, name=name, **extra_params)
 
     def is_first(self):
         """Returns True if the Link is the first component in this slot.
