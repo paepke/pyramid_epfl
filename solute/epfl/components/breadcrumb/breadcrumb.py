ceaae1e353c99e8c63f8baf4dbba8356120739fc
mah <mah@solute.de>
2015-06-24 11:40:56 +0200

    breadcump components: added route param again

--- solute/epfl/components/breadcrumb/breadcrumb.py
+++ solute/epfl/components/breadcrumb/breadcrumb.py
@@ -6,7 +6,7 @@ class Breadcrumb(Link):
 
     breadcrumb=True  #: The link is used as a breadcrumb per default.
 
-    def __init__(self, page, cid, url=None, name=None, icon=None, **extra_params):
+    def __init__(self, page, cid, url=None, route=None, name=None, icon=None, **extra_params):
         """Convenience component that can be used for creating breadcrumbs.
 
         Usage:
@@ -21,7 +21,8 @@ class Breadcrumb(Link):
             )
 
         :param url: The url the breadcrumb points to. Used as src attribute of the A-Tag.
+        :param route: The route this link points to. Used to look up the url for the src attribute of the A-Tag.
         :param name: The name displayed for this breadcrumb.
         :param icon: The icon to be displayed in front of the text.
         """
-        super(Breadcrumb, self).__init__(page, cid, url=url, name=name, icon=icon, **extra_params)
+        super(Breadcrumb, self).__init__(page, cid, url=url, route=route, name=name, icon=icon, **extra_params)
