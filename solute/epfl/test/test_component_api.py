535ed2913b1eb7c44f59b0b413d58b5a25547fb3
Justus Wingert <juw@solute.de>
2015-06-25 16:21:52 +0200

    Added container tests.

--- solute/epfl/test/test_component_api.py
+++ solute/epfl/test/test_component_api.py
@@ -35,6 +35,41 @@ def base_type(request, page, component_base_type_class):
     return page, tested_component, compo_info
 
 
+@pytest.fixture(params=['static', 'static_with_child', 'static_as_child', 'dynamic', 'dynamic_with_child'])
+def container_type(request, page, component_container_type_class):
+    root_node = ComponentContainerBase
+    if request.param == 'static':
+        root_node = component_container_type_class()
+    elif request.param == 'static_with_child':
+        root_node = component_container_type_class(
+            node_list=[ComponentBase(cid='child_compo')]
+        )
+    elif request.param == 'static_as_child':
+        root_node = ComponentContainerBase(
+            node_list=[
+                component_container_type_class(cid='tested_component')
+            ])
+
+    page.root_node = root_node
+    page.handle_transaction()
+
+    if request.param == 'dynamic':
+        page.root_node.add_component(
+            component_container_type_class(cid='tested_component')
+        )
+    elif request.param == 'dynamic_with_child':
+        page.root_node.add_component(
+            component_container_type_class(
+                cid='tested_component',
+                node_list=[ComponentBase()]
+            ))
+
+    try:
+        return page, page.tested_component, page.transaction.get_component('tested_component')
+    except AttributeError:
+        return page, page.root_node, page.transaction.get_component('root_node')
+
+
 def test_base_type(base_type):
     page, tested_node, compo_info = base_type
 
@@ -48,5 +83,20 @@ def test_base_type(base_type):
 
 def test_base_type_style(component_base_type_class):
     assert_style(component_base_type_class)
+
+
+def test_container_type(container_type):
+    page, tested_node, compo_info = container_type
+
+    assert_coherence(tested_node, compo_info)
+
+    rendered_html = page.root_node.render()
+    rendered_js = page.root_node.render(target='js')
+
+    assert_rendering(compo_info, rendered_html, rendered_js)
+
+
+def test_container_type_style(component_container_type_class):
+    assert_style(component_container_type_class)
 
 
