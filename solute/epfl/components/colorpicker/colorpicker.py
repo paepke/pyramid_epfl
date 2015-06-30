7bc6c435f7a3c8bae077d7706b78d50570232fb0
Marc Stipcevic <mast@solute.de>
2015-06-30 14:31:06 +0200

    Colorpicker:First Functions

--- solute/epfl/components/colorpicker/colorpicker.py
+++ solute/epfl/components/colorpicker/colorpicker.py
@@ -6,13 +6,45 @@ from solute.epfl.components.form.form import FormInputBase
 
 
 class ColorPicker(FormInputBase):
-
-    js_name = FormInputBase.js_name + [("solute.epfl.components:colorpicker/static","colorpicker.js")]
-    css_name = FormInputBase.css_name + [("solute.epfl.components:colorpicker/static","colorpicker.css")]
+    js_name = FormInputBase.js_name + [("solute.epfl.components:colorpicker/static", "colorpicker.js")]
+    css_name = FormInputBase.css_name + [("solute.epfl.components:colorpicker/static", "colorpicker.css")]
     template_name = "colorpicker/colorpicker.html"
-
 
     new_style_compo = True
 
     compo_js_params = ['fire_change_immediately']
     compo_js_name = 'ColorPicker'
+    compo_js_extras = ['handle_click']
+
+    TYPE_RGB = 0
+    TYPE_SPECIAL = 1
+
+    compo_state = FormInputBase.compo_state + ["value_options"]
+
+    # value = []
+
+    value_options = [
+        {"data": "#FF0000", "type": TYPE_RGB, "text": "Rot"},
+        {"data": "Silber", "type": TYPE_SPECIAL, },
+        {"data": "Bronze", "type": TYPE_SPECIAL, },
+        {"data": "Gold", "type": TYPE_SPECIAL, },
+        {"data": "#00FF00", "type": TYPE_RGB, },
+        {"data": "#0000FF", "type": TYPE_RGB, },
+        {"data": "#00FFFF", "type": TYPE_RGB, },
+        {"data": "#FFFFFF", "type": TYPE_RGB, },
+        {"data": "#000000", "type": TYPE_RGB, },
+        {"data": "#FF00FF", "type": TYPE_RGB, "text": "Pink"},
+    ]
+
+    def handle_change(self, value):
+        if self.value is None:
+            self.value = []
+
+        # check if value is in self value if true remove else add
+        full_value = [val for val in self.value_options if val["data"] == value][0]
+        if full_value in self.value:
+            self.value.remove(full_value)
+        else:
+            self.value.append(full_value)
+
+        self.redraw()
--- solute/epfl/components/colorpicker/static/colorpicker.js
+++ solute/epfl/components/colorpicker/static/colorpicker.js
@@ -1,5 +1,27 @@
-epfl.ColorPicker = function(cid, params) {
+epfl.ColorPicker = function (cid, params) {
     epfl.ComponentBase.call(this, cid, params);
-    console.log("COLORPICKER");
+    $('#' + cid + ' [data-toggle="tooltip"]').tooltip();
+     console.log("RELOAD");
 };
-epfl.ColorPicker.inherits_from(epfl.ComponentBase);
+epfl.ColorPicker.inherits_from(epfl.FormInputBase);
+
+epfl.ColorPicker.prototype.handle_local_click = function (event) {
+    console.log("CLICK");
+    epfl.FormInputBase.prototype.handle_local_click.call(this, event);
+
+    var value = null;
+
+    if ($(event.target).hasClass("epfl-colorpicker-specialfield") ||
+            $(event.target).hasClass("epfl-colorpicker-colorfield")) {
+        value = $(event.target).data("value");
+    } else if ($(event.target).hasClass("fa fa-code fa-lg")) {
+        value = $(event.target).parent().data("value");
+    }
+
+    if (value !== null) {
+        epfl.send(epfl.make_component_event(this.cid, "change", {
+            "value": value
+        }));
+    }
+
+};
