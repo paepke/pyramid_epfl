75e1ebd3b1c35c5ffd6aefc69f043a4b035b9aa2
Marc Stipcevic <mast@solute.de>
2015-07-06 15:54:11 +0200

    Write Tests for colorpicker and color thief

--- solute/epfl/test/components/test_colorpicker.py
+++ solute/epfl/test/components/test_colorpicker.py
@@ -1,3 +1,96 @@
 # * encoding: utf-8
 
 from __future__ import unicode_literals
+from solute.epfl import components
+
+
+def test_render_with_no_options(page):
+    page.root_node = components.ColorPicker(
+        value_options=[]
+    )
+    page.handle_transaction()
+
+    compo = page.root_node
+
+    assert 'class="epfl-colorpicker"' in compo.render(), 'general rendering problem class'
+    assert 'id="%s' % compo.cid in compo.render(), 'general rendering problem id'
+    assert 'epflid="%s"' % compo.cid in compo.render(), 'general rendering problem epflid'
+    assert 'class="epfl-colorpicker-colorfield"' not in compo.render(), "a field got rendered where no one is excepted"
+    assert 'class="epfl-colorpicker-specialfield"' not in compo.render(), "a field got rendered where no one is excepted"
+
+
+def test_render_with_rgb_values(page):
+    page.root_node = components.ColorPicker(
+        value_options=[{"data": "#FFFFFF", "type": components.ColorPicker.TYPE_RGB, "text": "white"},
+                       {"data": "#000000", "type": components.ColorPicker.TYPE_RGB, "text": "black"}]
+    )
+
+    page.handle_transaction()
+
+    compo = page.root_node
+    assert 'class="epfl-colorpicker-colorfield pull-left"' in compo.render(), "no rgb color field found"
+    assert 'title="#FFFFFF white"' in compo.render(), "white color field not found"
+    assert 'title="#000000 black"' in compo.render(), "black color field not found"
+    assert 'style="background-color: #FFFFFF"' in compo.render(), "white color field not found by style"
+    assert 'style="background-color: #000000"' in compo.render(), "black color field not found by style"
+
+
+def test_render_with_special_values(page):
+    page.root_node = components.ColorPicker(
+        value_options=[{"data": "transparent", "type": components.ColorPicker.TYPE_SPECIAL, "text": "transparent"},
+                       {"data": "color", "type": components.ColorPicker.TYPE_SPECIAL, "text": "color"}]
+    )
+
+    page.handle_transaction()
+
+    compo = page.root_node
+    assert 'class="epfl-colorpicker-specialfield pull-left text-center"' in compo.render(), "no special field found"
+    assert 'title="transparent transparent"' in compo.render(), "transparent field not found"
+    assert 'title="color color"' in compo.render(), "color field not found"
+
+
+def test_change(page):
+    page.root_node = components.ColorPicker(
+        value_options=[{"data": "#FFFFFF", "type": components.ColorPicker.TYPE_RGB, "text": "white"},
+                       {"data": "#000000", "type": components.ColorPicker.TYPE_RGB, "text": "black"},
+                       {"data": "transparent", "type": components.ColorPicker.TYPE_SPECIAL, "text": "transparent"},
+                       {"data": "color", "type": components.ColorPicker.TYPE_SPECIAL, "text": "color"}]
+    )
+
+    page.handle_transaction()
+    compo = page.root_node
+
+    assert compo.get_value() == None, "wrong default value"
+    assert '<i class="fa fa-check fa-lg text-primary"></i>' not in compo.render(), "check icon found before handle change"
+
+    compo.handle_change("#FFFFFF")  # Click the white color field
+    page.handle_transaction()
+    compo.render_cache = None
+
+    assert compo.get_value() == [
+        {u'type': 0, u'data': u'#FFFFFF', u'text': u'white'}], "value incorrect after handle change"
+    assert '<i class="fa fa-check fa-lg text-primary"></i>' in compo.render(), "check icon not found after handle change"
+
+    compo.handle_change("transparent")  # click the transparent special color field
+    page.handle_transaction()
+    compo.render_cache = None
+
+    assert compo.get_value() == [{u'type': 0, u'data': u'#FFFFFF', u'text': u'white'},
+                                 {u'type': 1, u'data': u'transparent',
+                                  u'text': u'transparent'}], "value incorrect after handle change"
+    assert 'style="background-color:#00FF00"' in compo.render(), "check color not found after handle change"
+
+    compo.handle_change("#FFFFFF")  # click the white color field again to remove it from selection
+    page.handle_transaction()
+    compo.render_cache = None
+
+    assert compo.get_value() == [
+        {u'type': 1, u'data': u'transparent', u'text': u'transparent'}], "value incorrect after handle change"
+    assert '<i class="fa fa-check fa-lg text-primary"></i>' not in compo.render(), "check icon not found after handle change"
+
+    compo.handle_change("transparent")  # click the transparent special color field again to remove it from selection
+    page.handle_transaction()
+    compo.render_cache = None
+
+    assert compo.get_value() == [], "value incorrect after handle change"
+    assert 'style="background-color:#00FF00"' not in compo.render(), "check color not found after handle change"
--- solute/epfl/test/components/test_colorthief.py
+++ solute/epfl/test/components/test_colorthief.py
@@ -1,3 +1,96 @@
 # * encoding: utf-8
 
 from __future__ import unicode_literals
+from lxml import etree
+
+from solute.epfl import components
+
+
+def test_render_with_no_options(page):
+    page.root_node = components.ColorThief(
+    )
+    page.handle_transaction()
+
+    compo = page.root_node
+    compo_html = etree.fromstring(compo.render())
+    assert compo_html.attrib.get('epflid', None) == "%s" % compo.cid, "epflid not found"
+    assert compo_html.attrib.get('id', None) == "%s" % compo.cid, "epflid not found"
+    assert compo_html.attrib.get('class', None) == "epfl-colorthief", "epflid not found"
+
+    dropzone = compo_html.find("div[@class='epfl-colorthief-dropzone text-center']")
+    assert dropzone is not None, "No Dropzone found"
+    assert dropzone.find("i") is not None, "No icon in Dropzone found"
+
+    img = compo_html.find("img[@class='epfl-colorthief-image']")
+    assert img is not None, "Could not found image"
+    assert img.attrib.get("data-cid", None) == "%s" % compo.cid, "image has no or wrong data-cid attribute"
+
+
+def test_render_with_options(page):
+    page.root_node = components.ColorThief(
+        height=250,
+        width=150,
+        image_src="test.png"
+    )
+    page.handle_transaction()
+
+    compo = page.root_node
+    compo_html = etree.fromstring(compo.render())
+    assert compo_html.attrib.get("style", None) == "height:250px; width:150px;", "Height and Width not set"
+    remove_icon = compo_html.find("i[@class='fa fa-times fa-lg color-danger epfl-colorthief-remove-icon']")
+    assert remove_icon is not None, "No remove icon found"
+
+    img = compo_html.find("img[@class='epfl-colorthief-image']")
+    assert img is not None, "Could not found image"
+    assert img.attrib.get("src", None) == "test.png", "Image src not set"
+
+
+def test_change(page):
+    page.root_node = components.ColorThief(
+    )
+    page.handle_transaction()
+
+    compo = page.root_node
+    compo_html = etree.fromstring(compo.render())
+
+    assert compo.get_value() is None, "Wrong default value"
+    assert compo.image_src is None, "Wrong default image_src"
+
+    compo.handle_change(value=[(255, 255, 255), (0, 0, 0)], image_src="test.png")
+
+    page.handle_transaction()
+    compo.render_cache = None
+    compo_html = etree.fromstring(compo.render())
+
+    assert compo.get_value() == [{u'rgb': u'#ffffff', u'selected': False},
+                                 {u'rgb': u'#000', u'selected': False}], "Wrong value after handle change"
+    assert compo.image_src == "test.png", "wrong image_src after handle_change"
+
+    color_divs = compo_html.findall("div[@class='epfl-colorthief-color pull-left text-center']")
+    assert len(color_divs) == 2, "No or not all color divs got renderd"
+    assert color_divs[0].attrib.get("style", None) == "background-color: #ffffff", "white color div has no color"
+    assert color_divs[1].attrib.get("style", None) == "background-color: #000", "black color div has no color"
+    assert color_divs[0].attrib.get("data-color", None) == "#ffffff", "white color div has no data-color"
+    assert color_divs[1].attrib.get("data-color", None) == "#000", "black color div has no data-color"
+
+    compo.handle_click_color("#000")
+    page.handle_transaction()
+    compo.render_cache = None
+    compo_html = etree.fromstring(compo.render())
+
+    assert compo.get_value() == [{u'rgb': u'#ffffff', u'selected': False},
+                                 {u'rgb': u'#000', u'selected': True}], "Wrong value after handle_click_color"
+
+    color_divs = compo_html.findall("div[@class='epfl-colorthief-color pull-left text-center']")
+    assert len(color_divs[1]) == 1, "Selected color div has no icon"
+
+    compo.handle_click_color("#000")
+    page.handle_transaction()
+    compo.render_cache = None
+    compo_html = etree.fromstring(compo.render())
+
+    assert compo.get_value() == [{u'rgb': u'#ffffff', u'selected': False},
+                                 {u'rgb': u'#000', u'selected': False}], "Wrong value after handle_click_color"
+
+    color_divs = compo_html.findall("div[@class='epfl-colorthief-color pull-left text-center']")
+    assert len(color_divs[1]) == 0, "Selected color div has a icon after deselect"
