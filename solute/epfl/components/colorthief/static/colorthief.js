ae05d25773e4764f4725f5cf9852ce2990997022
Marc Stipcevic <mast@solute.de>
2015-07-07 10:55:27 +0200

    Colorthief cleanup

--- solute/epfl/components/colorthief/static/colorthief.js
+++ solute/epfl/components/colorthief/static/colorthief.js
@@ -3,21 +3,14 @@ epfl.ColorThief = function (cid, params) {
 };
 epfl.ColorThief.inherits_from(epfl.ComponentBase);
 
-
-epfl.ColorThief.prototype.handle_drop_file = function (files, event) {
-};
-
-
-epfl.ColorThief.prototype.handle_drop_url = function (url, event) {
-};
-
 epfl.ColorThief.prototype.handle_click = function (event) {
     epfl.ComponentBase.prototype.handle_click.call(this, event);
-    if ($(event.target).hasClass("epfl-colorthief-color")) {
-        epfl.send(epfl.make_component_event(this.cid, "click_color", {"color": $(event.target).data("color")}));
-    }else if($(event.target).hasClass("fa-check")){
-        epfl.send(epfl.make_component_event(this.cid, "click_color", {"color": $(event.target).parent().data("color")}));
-    }else if($(event.target).hasClass("epfl-colorthief-remove-icon")){
+    var target = $(event.target);
+    if (target.hasClass("epfl-colorthief-color")) {
+        epfl.send(epfl.make_component_event(this.cid, "click_color", {"color": target.data("color")}));
+    }else if(target.hasClass("fa-check")){
+        epfl.send(epfl.make_component_event(this.cid, "click_color", {"color": target.parent().data("color")}));
+    }else if(target.hasClass("epfl-colorthief-remove-icon")){
         epfl.send(epfl.make_component_event(this.cid, "change", {"value": null, "image_src": null}));
     }
 };
@@ -32,5 +25,4 @@ epfl.ColorThief.prototype.handle_drop = function (event) {
         var dominantColors = colorThief.getPalette(image[0], parseInt(this.params["color_count"]));
         epfl.send(epfl.make_component_event(this.cid, "change", {"value": dominantColors, "image_src": imageSrc}));
     }
-
 };
