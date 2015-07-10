4c74fdd1e0935ed9d6d59fd0a6eb75900ecce96e
Marc Stipcevic <mast@solute.de>
2015-07-10 11:45:48 +0200

    New Colorthief Implemetation using mmcq algorithm

--- solute/epfl/components/colorthief/colorthief.py
+++ solute/epfl/components/colorthief/colorthief.py
@@ -3,130 +3,7 @@
 from solute.epfl.components.form.form import FormInputBase
 from urllib2 import urlopen
 import io
-import random
-
-try:
-    from PIL import Image
-    import numpy
-    dependency_error = False
-except ImportError:
-    dependency_error = True
-
-
-# Source: http://blog.zeevgilovitz.com/detecting-dominant-colours-in-python/
-#################################################
-class Cluster(object):
-    def __init__(self):
-        self.pixels = []
-        self.centroid = None
-
-    def addPoint(self, pixel):
-        self.pixels.append(pixel)
-
-    def setNewCentroid(self):
-
-        R = [colour[0] for colour in self.pixels]
-        G = [colour[1] for colour in self.pixels]
-        B = [colour[2] for colour in self.pixels]
-        try:
-            R = sum(R) / len(R)
-        except ZeroDivisionError:
-            R = 0
-        try:
-            G = sum(G) / len(G)
-        except ZeroDivisionError:
-            G = 0
-        try:
-            B = sum(B) / len(B)
-        except ZeroDivisionError:
-            B = 0
-
-        self.centroid = (R, G, B)
-        self.pixels = []
-
-        return self.centroid
-
-# Source: http://blog.zeevgilovitz.com/detecting-dominant-colours-in-python/
-#################################################
-class Kmeans(object):
-    def __init__(self, k=3, max_iterations=5, min_distance=5.0, size=200):
-        self.k = k
-        self.max_iterations = max_iterations
-        self.min_distance = min_distance
-        self.size = (size, size)
-
-    def run(self, image):
-        self.image = image
-        self.image.thumbnail(self.size)
-        self.pixels = numpy.array(image.getdata(), dtype=numpy.uint8)
-
-        self.clusters = [None for i in range(self.k)]
-        self.oldClusters = None
-
-        randomPixels = random.sample(self.pixels, self.k)
-
-        for idx in range(self.k):
-            self.clusters[idx] = Cluster()
-            self.clusters[idx].centroid = randomPixels[idx]
-
-        iterations = 0
-
-        while self.shouldExit(iterations) is False:
-
-            self.oldClusters = [cluster.centroid for cluster in self.clusters]
-
-            for pixel in self.pixels:
-                self.assignClusters(pixel)
-
-            for cluster in self.clusters:
-                cluster.setNewCentroid()
-
-            iterations += 1
-
-        return [cluster.centroid for cluster in self.clusters]
-
-    def assignClusters(self, pixel):
-        shortest = float('Inf')
-        for cluster in self.clusters:
-            distance = self.calcDistance(cluster.centroid, pixel)
-            if distance < shortest:
-                shortest = distance
-                nearest = cluster
-
-        nearest.addPoint(pixel)
-
-    def calcDistance(self, a, b):
-        result = numpy.sqrt(sum((a - b) ** 2))
-        return result
-
-    def shouldExit(self, iterations):
-        if self.oldClusters is None:
-            return False
-
-        for idx in range(self.k):
-            dist = self.calcDistance(
-                numpy.array(self.clusters[idx].centroid),
-                numpy.array(self.oldClusters[idx])
-            )
-            if dist < self.min_distance:
-                return True
-
-        if iterations <= self.max_iterations:
-            return False
-
-        return True
-
-
-def get_dominant_colors_from_url(url, color_count=8):
-    """Fetch the image from the url and extract the dominant colors
-
-    :param url: image url
-    :param color_count: count of dominant colors
-    """
-    path = io.BytesIO(urlopen(url).read())
-    im_ex = Image.open(path)
-    kk = Kmeans(k=color_count)
-    return kk.run(im_ex)
+from solute.epfl.components.colorthief.mmcq import get_palette
 
 
 class ColorThief(FormInputBase):
@@ -158,14 +35,19 @@ class ColorThief(FormInputBase):
         :param color_count: Count of colors which got extracted from the image
         :return:
         """
-        if dependency_error:
-            raise ImportError("ColorThief Component has pillow and numpy as dependency")
         super(ColorThief, self).__init__(page=page, cid=cid, height=height, width=width, image_src=image_src,
                                          color_count=color_count, **extra_params)
 
+    def __new__(cls, *args, **config):
+        super(ColorThief, cls).__new__(cls,*args,**config)
+        try:
+            import PIL
+        except ImportError:
+            raise ImportError("Colorthief needs pillow, check: http://pillow.readthedocs.org/installation.html#basic-installation ")
+
     def handle_change(self, value, image_src=None):
         if image_src is not None:
-            dominant_colors = set(get_dominant_colors_from_url(image_src, color_count=self.color_count))
+            dominant_colors = set(self.get_dominant_colors_from_url(image_src, color_count=self.color_count))
             self.value = [{"rgb": "#%x%x%x" % (val[0], val[1], val[2]), "selected": False} for val in dominant_colors]
         else:
             self.value = None
@@ -182,3 +64,13 @@ class ColorThief(FormInputBase):
                 break
 
         self.redraw()
+
+    def get_dominant_colors_from_url(self, url, color_count=8):
+        """Fetch the image from the url and extract the dominant colors
+
+        :param url: image url
+        :param color_count: count of dominant colors
+        """
+        bytes = io.BytesIO(urlopen(url).read())
+        with get_palette(blob=bytes, color_count=color_count) as palette:
+            return palette
