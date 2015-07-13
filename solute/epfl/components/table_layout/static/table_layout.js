5517d546d86fb617029061d2229f8b78d7fae361
Justus Wingert <juw@solute.de>
2015-07-13 15:00:15 +0200

    Bugfix: TableLayout did not correctly use its js parent.

--- solute/epfl/components/table_layout/static/table_layout.js
+++ solute/epfl/components/table_layout/static/table_layout.js
@@ -1,5 +1,5 @@
-epfl.TableLayout = function(cid, params) {
-    epfl.ComponentBase.call(this, cid, params);
+epfl.TableLayout = function (cid, params) {
+    epfl.PaginatedListLayout.call(this, cid, params);
     var compo = this;
     var selector = "#" + cid;
     if (params.fixed_header) {
@@ -15,26 +15,29 @@ epfl.TableLayout = function(cid, params) {
         setTimeout(compo.enable_fixed_header_table, 0);
     }
 };
-epfl.TableLayout.inherits_from(epfl.ComponentBase);
+
+epfl.TableLayout.inherits_from(epfl.PaginatedListLayout);
+
 Object.defineProperty(epfl.TableLayout.prototype, 'hide_column_icon', {
     get: function () {
-        res = this.elm.find('.hide-column-icon');
+        var res = this.elm.find('.hide-column-icon');
         return res;
     }
 });
+
 Object.defineProperty(epfl.TableLayout.prototype, 'show_column_icon', {
     get: function () {
-        res = this.elm.find('.show-column-icon');
+        var res = this.elm.find('.show-column-icon');
         return res;
     }
 });
 
 epfl.TableLayout.prototype.handle_click = function (event) {
-    epfl.ComponentBase.prototype.handle_click.call(this, event);
-    if (this.hide_column_icon.is(event.target) ) {
+    epfl.PaginatedListLayout.prototype.handle_click.call(this, event);
+    if (this.hide_column_icon.is(event.target)) {
         var parent_col = event.target.closest("th");
         this.send_event("hide_column", {column_index: $(parent_col).index()});
-    } else if (this.show_column_icon.is(event.target) ) {
+    } else if (this.show_column_icon.is(event.target)) {
         var parent_col = event.target.closest("th");
         this.send_event("show_column", {column_index: $(parent_col).index()});
     }
