ef0ad3d49a8bde79df70198102cd953653b32165
svh <svh@solute.de>
2015-07-13 15:48:00 +0200

    Updated ButtonComponent to be a new-style component.

--- solute/epfl/components/button/button.js
+++
@@ -1,5 +0,0 @@
-epfl.init_component("{{ compo.cid }}", "Button", {"event_name" : "{{ compo.event_name }}",
-                                                  "event_target" : "{{ compo.event_target }}",
-                                                  "confirm_first": {{ compo.confirm_first|format_bool }},
-                                                  "confirm_message": "{{ compo.confirm_message }}",
-                                                  "disable_on_click": {{ compo.disable_on_click|format_bool }},});
--- solute/epfl/components/button/static/button.js
+++ solute/epfl/components/button/static/button.js
@@ -1,20 +1,21 @@
 epfl.Button = function(cid, params) {
     epfl.ComponentBase.call(this, cid, params);
-    var confirm_first=params["confirm_first"];
-    var confirm_message=params["confirm_message"];
-    var event_name=params["event_name"];
-    var event_target=params["event_target"];
-    var disable_on_click=params["disable_on_click"];
-    $('#' + cid).click(function(event) {
-        if (confirm_first && (!confirm(confirm_message))) {
-            return;
-        }
-        if (disable_on_click) {
-            $(this).addClass("disabled");
-        }
-        var request = epfl.make_component_event(event_target, event_name);
-        epfl.send(request);
-    });
-
 };
+
 epfl.Button.inherits_from(epfl.ComponentBase);
+
+epfl.Button.prototype.handle_click = function(event) {
+    // No super since handle_local_click is not required here
+
+    if (this.params["confirm_first"] && (!confirm(this.params["confirm_message"]))) {
+        return;
+    }
+    if (this.params["disable_on_click"]) {
+        $(this).addClass("disabled");
+    }
+    if(this.params["event_target"]) {
+        epfl.send(epfl.make_component_event(this.params["event_target"], this.params["event_name"]));
+        return;
+    }
+    this.send_event(this.params["event_name"], {});
+};
