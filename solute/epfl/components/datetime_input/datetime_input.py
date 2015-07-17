c7fe4278d9c5d29267baf81ad52d56b1e28e4590
Marc Stipcevic <mast@solute.de>
2015-07-17 11:52:03 +0200

    Datetime_input: New Bootstrap Datetime input

--- solute/epfl/components/datetime_input/datetime_input.py
+++ solute/epfl/components/datetime_input/datetime_input.py
@@ -1,48 +1,57 @@
 # * encoding: utf-8
 
-from solute.epfl.components.text_input.text_input import TextInput
+from solute.epfl.components.form.form import FormInputBase
 
 
-class DatetimeInput(TextInput):
-    #: Set to true if input field should be a datetime picker (using jquery-datetimepicker plugin)
-    date = True
+class DatetimeInput(FormInputBase):
+    js_name = FormInputBase.js_name + [("solute.epfl.components:datetime_input/static", "datetime_input.js"),
+                                       ("solute.epfl.components:datetime_input/static", "moment-with-locales.min.js"),
+                                       ("solute.epfl.components:datetime_input/static",
+                                        "bootstrap-datetimepicker.min.js")]
+
+    css_name = FormInputBase.css_name + [("solute.epfl.components:datetime_input/static",
+                                          "bootstrap-datetimepicker.min.css")]
+    template_name = "datetime_input/datetime_input.html"
+    compo_state = FormInputBase.compo_state + ["date_format"]
+    js_parts = []
+
+    date_format = "LLL"  #: This is the date format from moment.js http://momentjs.com/
+
+    new_style_compo = True
+    compo_js_params = ['fire_change_immediately', 'date_format']
+    compo_js_name = 'DatetimeInput'
 
     def __init__(self, page, cid,
+                 date_format=None,
                  label=None,
                  name=None,
                  value=None,
-                 default=None,
                  placeholder=None,
-                 readonly=False,
-                 validation_error='',
-                 mandatory=False,
-                 submit_form_on_enter=False,
-                 input_focus=False,
-                 fire_change_immediately=False,
-                 compo_col=12,
-                 label_col=2,
-                 layout_vertical=False,
+                 readonly=None,
+                 validation_error=None,
+                 validation_type=None,
+                 validation_helper=None,
+                 mandetory=None,
+                 input_focus=None,
+                 fire_change_immediately=None,
+                 compo_col=None,
+                 label_col=None,
+                 layout_vertical=None,
                  label_style=None,
                  input_style=None,
-                 typeahead=False,
-                 max_length=None,
-                 show_count=False,
-                 password=False,
-                 layover_icon=None,
-                 date=False,
                  **extra_params):
-        """
-        Datetime input this is a convenience component for textinput which overrides the date flag to true,
+        """Datetime Input using bootstrap datime picker and moment js
 
+        :param date_format: #: This is the date format from moment.js http://momentjs.com/
         :param label: Optional label describing the input field
         :param name: An element without a name cannot have a value
         :param value: The actual value of the input element that is posted upon form submission
-        :param default: Default value that may be pre-set or pre-selected
         :param placeholder: Placeholder text that can be displayed if supported by the input
         :param readonly: Set to true if input cannot be changed and is displayed in readonly mode
         :param validation_error: Set during call of :func:`validate` with an error message if validation fails
+        :param validation_type: Form validation selector.
+        :param validation_helper: Subclasses can add their own validation helper lamdbas in order to extend validation logic.
         :param mandatory: Set to true if value has to be provided for this element in order to yield a valid form
-        :param submit_form_on_enter: If true, underlying form is submitted upon enter key in this input
         :param input_focus: Set focus on this input when component is displayed
         :param fire_change_immediately: Set to true if input change events should be fired immediately to the server. Otherwise, change events are fired upon the next immediate epfl event
         :param compo_col: Set the width of the complete input component (default: max: 12)
@@ -50,23 +59,18 @@ class DatetimeInput(TextInput):
         :param layout_vertical: Set to true if label should be displayed on top of the input and not on the left before it
         :param label_style: Can be used to add additional css styles for the label
         :param input_style: Can be used to add additional css styles for the input
-        :param typeahead: Set to true if typeahead should be provided by the input (if supported)
-        :param max_length: Maximum length for the input
-        :param show_count: Set to true to show a input counter right to the field. Requires a max_length to be set
-        :param password: Set to true if input field should be used as a password field
-        :param layover_icon: Optional font-awesome icon to be rendered as a layover icon above the input field (aligned to the right)
-        :param date: Set to true if input field should be a datetime picker (using jquery-datetimepicker plugin)
         """
-        super(DatetimeInput, self).__init__(page, cid,
+        super(DatetimeInput, self).__init__(page=page, cid=cid,
+                                            date_format=date_format,
                                             label=label,
                                             name=name,
                                             value=value,
-                                            default=default,
                                             placeholder=placeholder,
                                             readonly=readonly,
                                             validation_error=validation_error,
-                                            mandatory=mandatory,
-                                            submit_form_on_enter=submit_form_on_enter,
+                                            validation_type=validation_type,
+                                            validation_helper=validation_helper,
+                                            mandetory=mandetory,
                                             input_focus=input_focus,
                                             fire_change_immediately=fire_change_immediately,
                                             compo_col=compo_col,
@@ -74,9 +78,5 @@ class DatetimeInput(TextInput):
                                             layout_vertical=layout_vertical,
                                             label_style=label_style,
                                             input_style=input_style,
-                                            typeahead=typeahead,
-                                            max_length=max_length,
-                                            show_count=show_count,
-                                            password=password,
-                                            layover_icon=layover_icon,
                                             **extra_params)
+
---
+++ solute/epfl/components/datetime_input/static/datetime_input.js
@@ -0,0 +1,44 @@
+epfl.DatetimeInput = function (cid, params) {
+    epfl.FormInputBase.call(this, cid, params);
+};
+epfl.DatetimeInput.inherits_from(epfl.FormInputBase);
+
+Object.defineProperty(epfl.DatetimeInput.prototype, 'input', {
+    get: function () {
+        return this.elm.find('input');
+    }
+});
+
+epfl.DatetimeInput.prototype.after_response = function (data) {
+    epfl.FormInputBase.prototype.after_response.call(this, data);
+    this.input.datetimepicker({
+        locale:'de',
+        format:this.params["date_format"]
+    }).blur(this.change.bind(this)).change(this.change.bind(this));
+};
+
+epfl.DatetimeInput.prototype.change = function (event) {
+    var value = this.input.val();
+    var enqueue_event = true;
+    if (this.params.fire_change_immediately) {
+        enqueue_event = false;
+    }
+    var parent_form = this.elm.closest('.epfl-form');
+    if (parent_form.length == 1) {
+        var is_dirty = parent_form.data('dirty');
+        if (is_dirty == '0') {
+            parent_form.data('dirty', '1');
+            // first change to the form. always send event immediately so that
+            // the serve can handle is_dirty change
+            enqueue_event = false;
+
+            this.repeat_enqueue('set_dirty', {}, this.cid + "_set_dirty");
+        }
+    }
+    if (enqueue_event) {
+        this.repeat_enqueue('change', {value: value}, this.cid + "_change");
+    } else {
+        this.send_event('change', {value: value});
+    }
+};
+
