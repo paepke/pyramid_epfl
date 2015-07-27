e18c8d9b78aefd48c76abe048d8136e325de6a25
Marc Stipcevic <mast@solute.de>
2015-07-27 11:38:26 +0200

    Add Youtube URL Validator and clean up other validator files

--- solute/epfl/validators/__init__.py
+++ solute/epfl/validators/__init__.py
@@ -1,9 +1,12 @@
+#* coding: utf-8
 from text import TextValidator
 from email import EmailValidator
 from number import NumberValidator
 from float import FloatValidator
+from youtube import YoutubeUrlValidator
 
 TextValidator.register_name('text')
 EmailValidator.register_name('email')
 NumberValidator.register_name('number')
 FloatValidator.register_name('float')
+YoutubeUrlValidator.register_name('youtube_url')
