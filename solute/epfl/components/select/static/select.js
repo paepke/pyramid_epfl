9aa8af27d8a1da1604142a3b250221c86485ddca
mah <mah@solute.de>
2015-07-28 10:08:17 +0200

    select component: move js logic into after_response method

--- solute/epfl/components/select/static/select.js
+++ solute/epfl/components/select/static/select.js
@@ -1,25 +1,28 @@
 epfl.Select = function (cid, params) {
     epfl.ComponentBase.call(this, cid, params);
+};
 
-    var selector = "#" + cid +"_input";
-    var compo = this;
-    var enqueue_event = !params.fire_change_immediately;
-    var submit_form_on_enter = params.submit_form_on_enter;
+epfl.Select.inherits_from(epfl.ComponentBase);
+
+epfl.Select.prototype.after_response = function (data) {
+    epfl.ComponentBase.prototype.after_response.call(this, data);
+
+    var obj = this;
+    var selector = "#" + obj.cid +"_input";
+    var enqueue_event = !obj.params.fire_change_immediately;
+    var submit_form_on_enter = obj.params.submit_form_on_enter;
 
     $(selector).change(function(){
-        epfl.FormInputBase.on_change(compo, $(this).val(), cid, enqueue_event);
+        epfl.FormInputBase.on_change(obj, $(this).val(), obj.cid, enqueue_event);
     });
 
     var keydown = function(event){
         if(event.which == 13){
-            epfl.FormInputBase.event_submit_form_on_enter(cid);
+            epfl.FormInputBase.event_submit_form_on_enter(obj.cid);
         }
     };
     var elm = $(selector);
     if(submit_form_on_enter){
         elm.keydown(keydown);
     }
-
 };
-
-epfl.Select.inherits_from(epfl.ComponentBase);
