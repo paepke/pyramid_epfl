769ed57b15ede0b0132d3d1c1404d41d7627a2d8
mah <mah@solute.de>
2015-07-28 15:23:26 +0200

    validators: replaced youtube validator with streaming video validator

--- solute/epfl/validators/__init__.py
+++ solute/epfl/validators/__init__.py
@@ -3,10 +3,10 @@ from text import TextValidator
 from email import EmailValidator
 from number import NumberValidator
 from float import FloatValidator
-from youtube import YoutubeUrlValidator
+from streaming_video import StreamingVideoUrlValidator
 
 TextValidator.register_name('text')
 EmailValidator.register_name('email')
 NumberValidator.register_name('number')
 FloatValidator.register_name('float')
-YoutubeUrlValidator.register_name('youtube_url')
+StreamingVideoUrlValidator.register_name("streaming_video_url")
