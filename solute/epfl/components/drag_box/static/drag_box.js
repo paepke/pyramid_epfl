6864a51f0e29ce1f43c249983957aa4d8789f3d5
Justus Wingert <juw@solute.de>
2015-07-29 10:39:15 +0200

    Removed first layer of deprecated components.

--- solute/epfl/components/drag_box/static/drag_box.js
+++
@@ -1,5 +0,0 @@
-epfl.DragBox = function(cid, params) {
-    epfl.BoxComponent.call(this, cid, params);
-};
-
-epfl.DragBox.inherits_from(epfl.Box);
--- solute/epfl/components/toggle_list_layout/toggle_list_layout.js
+++
@@ -1,9 +0,0 @@
-$('[epflid=' + '{{ compo.cid }}] > div > i.fa-minus')
-    .click(function () {
-        epfl.send(epfl.make_component_event('{{ compo.cid }}', 'hide'));
-    });
-
-$('[epflid=' + '{{ compo.cid }}] > div > i.fa-plus')
-    .click(function () {
-        epfl.send(epfl.make_component_event('{{ compo.cid }}', 'show'));
-    });
