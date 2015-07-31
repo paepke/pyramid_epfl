60ae334a8a639c2092fc446478d3d5a1b8a3e498
Justus Wingert <juw@solute.de>
2015-07-31 13:26:29 +0200

    Added documentation for Radio component.

--- solute/epfl/components/radio/radio.py
+++ solute/epfl/components/radio/radio.py
@@ -12,17 +12,21 @@ class Radio(FormInputBase):
         form = Form(node_list=[Radio(label="Gender:", name="gender", default="male", options=["male", "female"])])
 
     """
-
-    options = None
-
-    compo_state = FormInputBase.compo_state + ['options']
-
     template_name = "radio/radio.html"
-
-    validation_type = 'text'
-
-    js_parts = FormInputBase.js_parts[:]
-    js_parts.extend(['radio/radio.js'])
 
     js_name = FormInputBase.js_name + [("solute.epfl.components:radio/static", "radio.js")]
     css_name = FormInputBase.css_name + [("solute.epfl.components:radio/static", "radio.css")]
+
+    js_parts = FormInputBase.js_parts + ['radio/radio.js']
+
+    compo_state = FormInputBase.compo_state + ['options']
+
+    validation_type = 'text'  #: Validate as a text.
+    options = None  #: List of strings or key, value tuples to be used as options.
+
+    def __init__(self, page, cid, options=None, **extra_params):
+        """Simple radio form component.
+
+        :param options: List of strings or key, value tuples to be used as options.
+        """
+        super(Radio, self).__init__(page, cid, **extra_params)
