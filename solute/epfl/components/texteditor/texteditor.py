b8c0fada37ae1426e5329e76cd8ea957adf6597e
Justus Wingert <juw@solute.de>
2015-07-31 13:41:21 +0200

    Added docs to TextEditor.

--- solute/epfl/components/texteditor/texteditor.py
+++ solute/epfl/components/texteditor/texteditor.py
@@ -12,17 +12,25 @@ class TextEditor(FormInputBase):
         form = Form(node_list=[TextEditor(label="Provide a description:", name="description")])
     
     """
-
     js_parts = ['texteditor/texteditor.js']
     js_name = [('solute.epfl.components:texteditor/static', 'texteditor.js')]
     js_name_no_bundle = [('solute.epfl.components:texteditor/static', 'ckeditor.js')]
 
     template_name = "texteditor/texteditor.html"
 
-    validation_type = 'text'
+    validation_type = 'text'  #: Validate this component as text.
 
     #: The config-file that should be used for this instance. Available are at least "config" and "slimconfig"
     editor_config_file = "config"
 
     #: Set True to automatically remove all formatting on paste
     clean_paste = False
+
+    def __init__(self, page, cid, editor_config_file=None, clean_paste=None, **extra_params):
+        """A wysiwyg text editor.
+
+        :param editor_config_file: Config to be used can be either the default "config" or "slimconfig".
+        :param clean_paste: Automatically remove all formatting on paste.
+        """
+        super(TextEditor, self).__init__(page, cid, editor_config_file=editor_config_file, clean_paste=clean_paste,
+                                         **extra_params)
