a81695d728e4224d89a61f1a60ce1fd14d9475e9
Justus Wingert <juw@solute.de>
2015-07-31 12:21:15 +0200

    Updated docs for CardinalLayout.

--- solute/epfl/components/cardinal_layout/cardinal_layout.py
+++ solute/epfl/components/cardinal_layout/cardinal_layout.py
@@ -11,12 +11,19 @@ class CardinalLayout(epflcomponentbase.ComponentContainerBase):
     template_name = "cardinal_layout/cardinal_layout.html"
     asset_spec = "solute.epfl.components:cardinal_layout/static"
 
-    _cardinal_components = None
-
     compo_state = ['constrained']
 
-    constrained = None
+    _cardinal_components = None  #: Cache for the slotted components.
+    constrained = None  #: Constrain the layout to the center of the screen.
     plain = []  #: List of slots to be rendered as plain divs.
+
+    def __init__(self, page, cid, constrained=None, plain=None, **extra_params):
+        """A Layout component that displays it's children in panels for the four cardinal directions or a center panel.
+
+        :param constrained: If true the Cardinal Layout will restrict itself to the center of the screen.
+        :param plain: List of slots to be rendered as plain divs.
+        """
+        super(CardinalLayout, self).__init__(page, cid, constrained=constrained, plain=plain, **extra_params)
 
     def cardinal_components(self, direction='center'):
         if self._cardinal_components is None:
